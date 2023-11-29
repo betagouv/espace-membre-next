0cb007deb648950240febc10e9edfcfc6a17a204
Lucas Charrier <lucharrier@gmail.com>
2023-11-29 10:21:49 +0100

    fix: add signin client component

--- src/app/(public)/signin/page.tsx
+++ src/app/(public)/signin/page.tsx
@@ -1,44 +1,10 @@
-import axios from "axios";
 import { Metadata } from "next";
-import { useRouter, useSearchParams } from "next/navigation";
-import Button from "@codegouvfr/react-dsfr/Button";
-import routes, { computeRoute } from "@/routes/routes";
+import SignClientPage from "./SiginClientPage";
 
 export const metadata: Metadata = {
     title: "Espace-Membre: me connecter",
 };
 
 export default function SignIn() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const next = searchParams.get("next");
-    if (!window) {
-        return null;
-    }
-    const hash = window.location.hash.split("#")[1];
-    const onSubmit = async () => {
-        const value = await axios.post(computeRoute(routes.SIGNIN_API), {
-            token: hash,
-        });
-        await axios.get(computeRoute(routes.ME));
-        router.push("/account");
-    };
-
-    return (
-        <div>
-            <p>
-                Gère ton compte email (mot de passe, redirections, etc) et les
-                membres de la communauté (arrivées et départs).
-            </p>
-
-            <div>
-                <h4>
-                    <center>Connexion à l'espace membre</center>
-                </h4>
-                <center>
-                    <Button onClick={onSubmit}>Me connecter</Button>
-                </center>
-            </div>
-        </div>
-    );
+    return <SignClientPage></SignClientPage>;
 }
