245a0a4c195d72763fa187a5303fa7229624dd75
Lucas Charrier <lucharrier@gmail.com>
2023-12-06 17:45:42 +0100

    feat: add withCredentials and post form

--- src/app/(public)/signin/SiginClientPage.tsx
+++ src/app/(public)/signin/SiginClientPage.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import Button from "@codegouvfr/react-dsfr/Button";
 import routes, { computeRoute } from "@/routes/routes";
+import config from "@/config";
 
 export default function SignClientPage() {
     const router = useRouter();
@@ -13,11 +14,19 @@ export default function SignClientPage() {
     }
     const hash = window.location.hash.split("#")[1];
     const onSubmit = async () => {
-        const value = await axios.post(computeRoute(routes.SIGNIN_API), {
-            token: hash,
+        const value = await axios.post(
+            computeRoute(`${routes.SIGNIN_API}`),
+            {
+                token: hash,
+            },
+            {
+                withCredentials: true,
+            }
+        );
+        await axios.get(computeRoute(routes.ME), {
+            withCredentials: true,
         });
-        await axios.get(computeRoute(routes.ME));
-        router.push("/account");
+        // router.push("/account");
     };
 
     return (
@@ -34,6 +43,16 @@ export default function SignClientPage() {
                 <center>
                     <Button onClick={onSubmit}>Me connecter</Button>
                 </center>
+                <form action={`${config.API_SERVER}/${routes.SIGNIN}`}>
+                    <input hidden={true} type="text" name="next" value={""} />
+                    <input
+                        hidden={true}
+                        type="text"
+                        name="token"
+                        value={hash}
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         </div>
     );
