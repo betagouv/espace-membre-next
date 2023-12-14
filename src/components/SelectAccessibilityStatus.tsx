c5f765bd78948b41b5a7092e1c59af490a4d95c6
Lucas Charrier <lucharrier@gmail.com>
2023-12-14 17:40:08 +0100

    feat: add accessibility status component

---
+++ src/components/SelectAccessibilityStatus.tsx
@@ -0,0 +1,28 @@
+import Select from "@codegouvfr/react-dsfr/Select";
+import React from "react";
+
+const options = [
+    { value: "non conforme", label: "non conforme" },
+    { value: "partiellement conforme", label: "partiellement conforme" },
+    { value: "totalement conforme", label: "totalement conforme" },
+];
+export default ({ value, onChange }) => {
+    return (
+        <Select
+            label="Sélectionne l'accessibilité de votre produit"
+            nativeSelectProps={{
+                onChange,
+                defaultValue: value,
+                required: true,
+            }}
+        >
+            {options.map((option) => {
+                return (
+                    <option key={option.value} value={option.value}>
+                        {option.value}
+                    </option>
+                );
+            })}
+        </Select>
+    );
+};
