88115dc7557d17ed554c54b810980956fd1804b8
Lucas Charrier <lucharrier@gmail.com>
2023-12-20 18:51:11 +0100

    feat: update badge request page to change workflow

--- src/components/BadgePage/BadgePage.tsx
+++ src/components/BadgePage/BadgePage.tsx
@@ -1,99 +1,115 @@
-import React from "react";
 import { BadgeDossier } from "@/models/badgeDemande";
 import { BadgeRequest } from "@/models/badgeRequests";
-import { WelcomeScreen } from "./BadgeWelcomeScreen";
-import { BadgePendingView } from "./BadgePendingView";
-import { BadgeExistingView } from "./BadgeExistingView";
+import Alert from "@codegouvfr/react-dsfr/Alert";
 import Button from "@codegouvfr/react-dsfr/Button";
-
-enum STEP {
-    infoScreen = "infoScreen",
-    welcomeScreen = "welcomeScreen",
-    documentScreen = "documentScreen",
-    finalScreen = "finalScreen",
-}
+import { useState } from "react";
+import { WelcomeScreen } from "./BadgeWelcomeScreen";
+import { BadgeExistingView } from "./BadgeExistingView";
 
 export interface BadgeProps {
-    title: string;
-    currentUserId: string;
-    dossier: BadgeDossier;
-    errors: string[];
-    messages: string[];
-    activeTab: string;
-    request: Request;
-    formData: FormData;
     badgeRequest: BadgeRequest;
-    formValidationErrors: any;
-    startups: string[];
-    isAdmin: boolean;
-    firstName: string;
-    lastName: string;
-    attributaire: string;
-    endDate: Date;
-    primaryEmail: string;
+    dossier: BadgeDossier;
 }
 
-export const Badge = function (props: BadgeProps) {
-    const [step, setStep] = React.useState(
-        props.badgeRequest || props.dossier
-            ? STEP.documentScreen
-            : STEP.welcomeScreen
-    );
-    const [fixes] = React.useState([STEP.welcomeScreen, STEP.documentScreen]);
-    const [dsToken, setDSToken] = React.useState(
-        props.badgeRequest ? props.badgeRequest.ds_token : null
-    );
+const STATUS = {
+    "En attente de traitement": {
+        status: `En attente de traitement`,
+        description: `La demande est en attente de traitement. Tu recevras un email quand la demande sera traitée côté DINUM`,
+    },
+    "": {
+        status: `En attente de traitement`,
+        description: `La demande est en attente de traitement. Tu recevras un email quand la demande sera traitée côté DINUM`,
+    },
+    "En attente de validation par responsable incubateur": {
+        status: `En attente de validation par le responsable d'incubateur`,
+        description: `Ta demande a été traitée côté DINUM et en attente de validation par le responsable d'incubateur.`,
+    },
+    "En attente de création par bureau des badges": {
+        status: `En attente du bureau des badges`,
+        description: `Ta demande a été validée par le responsable d'incubateur et commandé auprès du bureau des badges.`,
+    },
+    "En attente de récupération": {
+        status: `En attente de récupération`,
+        description: `Tu peux aller chercher ton badge au bureau des badges qui est en attente de récupération.`,
+    },
+    Récupéré: {
+        status: `Récupéré`,
+        description: `Tu as récupéré ton badge.`,
+    },
+    "Formulaire non valide": {
+        status: `Formulaire non valide`,
+        description: `Ton formulaire est non valide tu as du recevoir un message dans tes emails.`,
+    },
+};
 
-    function goBack() {
-        const currentStepIndex = fixes.findIndex((s) => s === step);
-        const previousStep = fixes[currentStepIndex - 1] || STEP.welcomeScreen;
-        setStep(previousStep);
-    }
+export const Badge = function ({ badgeRequest, dossier }: BadgeProps) {
+    const [forceRequest, setForceRequest] = useState<boolean>(false);
 
-    function next() {
-        const currentStepIndex = fixes.findIndex((s) => s === step);
-        const nextStep = fixes[currentStepIndex + 1];
-        setStep(nextStep);
-        const state = {
-            step: nextStep,
-        };
-        history.pushState(state, "");
-    }
-
-    let stepView;
-    if (props.dossier && props.dossier.state === "accepte") {
-        stepView = <BadgeExistingView dossier={props.dossier} />;
-    } else if (step === STEP.welcomeScreen) {
-        stepView = (
-            <WelcomeScreen
-                setDSToken={setDSToken}
-                next={next}
-                dossier={props.dossier}
-                badgeRequest={props.badgeRequest}
-            />
+    function getStatus(dossier) {
+        const status = dossier.annotations.find(
+            (annotation) => annotation.label === "Status"
+        ).stringValue;
+        return (
+            <>
+                <p>
+                    <b>Statut du badge :</b>
+                    {STATUS[status].status}
+                </p>
+                <p>{STATUS[status].description}</p>
+            </>
         );
-    } else {
-        stepView = (
-            <BadgePendingView
-                dossier={props.dossier}
-                dsToken={dsToken}
-                primaryEmail={props.primaryEmail}
-            />
-        );
     }
+    console.log(dossier, badgeRequest);
     return (
-        <div>
-            {step !== STEP.welcomeScreen && (
-                <Button
-                    size="small"
-                    iconId="fr-icon-arrow-left-s-line"
-                    priority="tertiary no outline"
-                    onClick={() => goBack()}
-                >
-                    Retour
-                </Button>
+        <>
+            <h2>Demande de badge</h2>
+            {!!dossier && (
+                <>
+                    {dossier.state === "en_instruction" && (
+                        <Alert
+                            title="Vous avez déjà un badge en cours d'instruction"
+                            description={`Vous avez déjà un badge en cours de création, vous serez notifié par email quand le badge sera crée. Cela peux prendre une semaine.
+                            Si vous n'avez pas de nouvelle, merci de faire un message dans le canal ~bureau-segur de mattermost.`}
+                            severity="info"
+                        />
+                    )}
+                    {dossier.state === "accepte" && (
+                        <>
+                            <Alert
+                                title="Votre badge va être créé."
+                                description="Votre aller recevoir un email quand il sera disponible"
+                                severity="info"
+                            />
+                            <BadgeExistingView dossier={dossier} />
+                        </>
+                    )}
+
+                    <p>{getStatus(dossier)}</p>
+                    <br />
+
+                    {!forceRequest && (
+                        <>
+                            <p>
+                                Si tu penses qu'il y a une erreur, tu peux
+                                refaire une demande.
+                            </p>
+                            <Button
+                                priority="secondary"
+                                nativeButtonProps={{
+                                    onClick: () => {
+                                        setForceRequest(true);
+                                    },
+                                }}
+                            >
+                                Refaire une demande
+                            </Button>
+                        </>
+                    )}
+                </>
             )}
-            {stepView}
-        </div>
+            {(!dossier || forceRequest) && (
+                <WelcomeScreen badgeRequest={badgeRequest}></WelcomeScreen>
+            )}
+        </>
     );
 };
