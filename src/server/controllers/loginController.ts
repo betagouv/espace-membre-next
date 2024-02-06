b4f2966a4d2ea3d5f4753ec0c3da0dcd26e941c0
Lucas Charrier <lucharrier@gmail.com>
2024-02-06 18:15:55 +0100

    chore(config): separate cron env var from app env var

--- src/server/controllers/loginController.ts
+++ src/server/controllers/loginController.ts
@@ -140,9 +140,7 @@ export async function postLoginApi(req, res) {
     username = dbResponse.username;
 
     try {
-        const secretariatUrl = config.FRONT_APP_URL
-            ? `${config.FRONT_APP_URL}`
-            : `${config.protocol}://${req.get("host")}`;
+        const secretariatUrl = `${config.protocol}://${req.get("host")}`;
         const token = generateToken();
         const loginUrl: URL = new URL(secretariatUrl + "/signin" + `#${token}`);
         if (req.query.anchor) {
