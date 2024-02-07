feed716ef31be39dff8e37903d91b0123a033c6c
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-07 11:49:40 +0100

    feat: add sentry (#75)

--- next.config.js
+++ next.config.js
@@ -1,21 +1,42 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-      serverComponentsExternalPackages: ['knex'],
+        serverComponentsExternalPackages: ["knex"],
     },
-    webpack: (config, { isServer })  => {
+    sentry: {
+        disableServerWebpackPlugin: true,
+        disableClientWebpackPlugin: true,
+    },
+    webpack: (config, { isServer }) => {
         if (!isServer) {
             // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
             config.resolve.fallback = {
-                fs: false
-            }
+                fs: false,
+            };
         }
-      config.module.rules.push({
-        test: /\.woff2$/,
-        type: "asset/resource"
-      });
-      return config;
-    }
+        config.module.rules.push({
+            test: /\.woff2$/,
+            type: "asset/resource",
+        });
+        return config;
+    },
 };
-  
-module.exports = nextConfig
+
+const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+    //org: "example-org",
+    //project: "example-project",
+    // An auth token is required for uploading source maps.
+    //authToken: process.env.SENTRY_AUTH_TOKEN,
+    //silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
