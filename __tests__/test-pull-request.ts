8a0829bd1dffb9005e6397f865684fb7aa52c4a3
Lucas Charrier <lucharrier@gmail.com>
2024-02-07 14:41:48 +0100

    test: fix test for get pull requests endpoint which is private now

--- __tests__/test-pull-request.ts
+++ __tests__/test-pull-request.ts
@@ -4,19 +4,32 @@ import chaiHttp from "chai-http";
 
 import app from "@/server/index";
 import routes from "@/routes/routes";
+import utils from "./utils";
 import * as github from "@/lib/github";
+import * as session from "@/server/helpers/session";
+
 chai.use(chaiHttp);
 
-describe("GET pull request watchers", () => {
-    describe("GET /users/:username/email unauthenticated", () => {
-        it("should return an Unauthorized error", async () => {
-            let pullRequestStub = sinon.stub(github, "getPullRequests");
-            pullRequestStub.returns(Promise.resolve([]));
-            const res = await chai
-                .request(app)
-                .get(routes.PULL_REQUEST_GET_PRS);
-            pullRequestStub.called.should.be.true;
-            pullRequestStub.restore();
-        });
+describe("GET all opened pull requests unauthenticated", () => {
+    it("should return an unauthorized error", async () => {
+        let pullRequestStub = sinon.stub(github, "getPullRequests");
+        pullRequestStub.returns(Promise.resolve([]));
+        const res = await chai.request(app).get(routes.PULL_REQUEST_GET_PRS);
+        res.should.have.status(500);
+        pullRequestStub.called.should.be.false;
+        pullRequestStub.restore();
+    });
+
+    it("should get pull requests when use is logged", async () => {
+        const getToken = sinon.stub(session, "getToken");
+        getToken.returns(utils.getJWT("membre.actif"));
+
+        let pullRequestStub = sinon.stub(github, "getPullRequests");
+        pullRequestStub.returns(Promise.resolve([]));
+        const res = await chai.request(app).get(routes.PULL_REQUEST_GET_PRS);
+        res.should.have.status(200);
+        pullRequestStub.called.should.be.true;
+        pullRequestStub.restore();
+        getToken.restore();
     });
 });
