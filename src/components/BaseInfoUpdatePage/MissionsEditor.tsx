88115d7acee068f57cca4aa1d196695c22132616
Lucas Charrier <lucharrier@gmail.com>
2024-02-14 15:56:02 +0100

    chore(MissionsEditor): parse date as string when passing value to input

--- src/components/BaseInfoUpdatePage/MissionsEditor.tsx
+++ src/components/BaseInfoUpdatePage/MissionsEditor.tsx
@@ -5,6 +5,7 @@ import { Select } from "@codegouvfr/react-dsfr/Select";
 import {
     Control,
     useFieldArray,
+    useWatch,
     UseFormRegister,
     UseFormSetValue,
 } from "react-hook-form";
@@ -13,6 +14,166 @@ import { addMonths } from "date-fns/addMonths";
 import { StartupsPicker } from "./StartupsPicker";
 import { MemberSchemaType } from "./BaseInfoUpdate";
 import { userStatusOptions } from "@/frontConfig";
+
+const Mission = ({
+    index,
+    register,
+    control,
+    setValue,
+    mission,
+    missionsRemove,
+    onMissionAutoEndClick,
+    startupOptions,
+    errors,
+}) => {
+    const missionErrors = errors;
+    const defaultState = (field) => ({
+        state:
+            missionErrors && missionErrors[field]
+                ? ("error" as const)
+                : ("default" as const),
+        stateRelatedMessage: missionErrors && missionErrors[field]?.message,
+    });
+    const startDateValue = useWatch({
+        control,
+        name: `missions.${index}.start`,
+    });
+    // Convertir la valeur de date en format de chaîne requis par l'input de type date
+    const startDateString = startDateValue
+        ? new Date(startDateValue).toISOString().substring(0, 10)
+        : "";
+
+    const endDateValue = useWatch({
+        control,
+        name: `missions.${index}.end`,
+    });
+    const endDateString = endDateValue
+        ? new Date(endDateValue).toISOString().substring(0, 10)
+        : "";
+
+    return (
+        <div key={index} className={fr.cx("fr-mb-6w")}>
+            <div className={fr.cx("fr-text--heavy")}>
+                Mission {index + 1}
+                {index > 0 && (
+                    <button
+                        className={fr.cx("fr-icon-delete-bin-line")}
+                        style={{
+                            cursor: "pointer",
+                            float: "right",
+                        }}
+                        onClick={() => missionsRemove(index)}
+                        title={`Supprimer la mission ${index + 1}`}
+                    />
+                )}
+                <hr className={fr.cx("fr-mt-1w")} />
+            </div>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+                <div className={fr.cx("fr-col-3")}>
+                    <Input
+                        label="Date de début"
+                        hintText="Début de ta mission"
+                        nativeInputProps={{
+                            style: { width: 200 },
+                            placeholder: "JJ/MM/YYYY",
+                            type: "date",
+                            ...register(`missions.${index}.start`),
+                            value: startDateString,
+                        }}
+                        {...defaultState("start")}
+                    />
+                </div>{" "}
+                <div className={fr.cx("fr-col-4")}>
+                    <Input
+                        label="Date de fin"
+                        nativeInputProps={{
+                            style: { width: 200 },
+                            placeholder: "JJ/MM/YYYY",
+                            type: "date",
+                            ...register(`missions.${index}.end`),
+                            value: endDateString,
+                        }}
+                        hintText={
+                            <div>
+                                En cas de doute, mettre{" "}
+                                <button
+                                    className={fr.cx("fr-link", "fr-text--xs")}
+                                    onClick={() => onMissionAutoEndClick(index)}
+                                    role="button"
+                                    type="button"
+                                    title="Mettre la date de fin à +3 mois"
+                                >
+                                    J+3 mois
+                                </button>
+                            </div>
+                        }
+                        {...defaultState("end")}
+                    />
+                </div>
+            </div>
+
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+                <div className={fr.cx("fr-col-6")}>
+                    <Input
+                        label="Employeur"
+                        nativeInputProps={{
+                            placeholder: "ex: Scopyleft",
+                            ...register(`missions.${index}.employer`),
+                        }}
+                        {...defaultState("employer")}
+                    />
+                </div>
+                <div className={fr.cx("fr-col-6")}>
+                    <Select
+                        label="Statut"
+                        nativeSelectProps={{
+                            ...register(`missions.${index}.status`),
+                        }}
+                        {...defaultState("status")}
+                    >
+                        <option value="">Statut:</option>
+                        {userStatusOptions.map((option) => (
+                            <option
+                                key={option.key}
+                                selected={option.key === mission.status}
+                                value={option.key}
+                            >
+                                {option.name}
+                            </option>
+                        ))}
+                    </Select>{" "}
+                </div>
+            </div>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+                <div className={fr.cx("fr-col-12")}>
+                    <StartupsPicker
+                        name="startups"
+                        control={control}
+                        startups={startupOptions}
+                        label="Produits concernés par la mission :"
+                        defaultValue={startupOptions.filter(
+                            (s) =>
+                                mission.startups &&
+                                mission.startups.includes(s.value)
+                        )}
+                        onChange={(startups) => {
+                            setValue(
+                                `missions.${index}.startups`,
+                                startups.map((startup) => startup.value),
+                                {
+                                    shouldValidate: true,
+                                    shouldDirty: true,
+                                }
+                            );
+                        }}
+                        {...defaultState("startups")}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+    // La logique de rendu pour chaque mission va ici...
+};
 
 export const MissionsEditor = ({
     control,
@@ -67,163 +228,20 @@ export const MissionsEditor = ({
                     {errors.message}
                 </div>
             )}
-            {missionsFields.map((mission, index, all) => {
-                const missionErrors = errors && errors[index];
-                const defaultState = (field) => ({
-                    state:
-                        missionErrors && missionErrors[field]
-                            ? ("error" as const)
-                            : ("default" as const),
-                    stateRelatedMessage:
-                        missionErrors && missionErrors[field]?.message,
-                });
-                return (
-                    <div key={index} className={fr.cx("fr-mb-6w")}>
-                        <div className={fr.cx("fr-text--heavy")}>
-                            Mission {index + 1}
-                            {index > 0 && (
-                                <button
-                                    className={fr.cx("fr-icon-delete-bin-line")}
-                                    style={{
-                                        cursor: "pointer",
-                                        float: "right",
-                                    }}
-                                    onClick={() => missionsRemove(index)}
-                                    title={`Supprimer la mission ${index + 1}`}
-                                />
-                            )}
-                            <hr className={fr.cx("fr-mt-1w")} />
-                        </div>
-                        <div
-                            className={fr.cx(
-                                "fr-grid-row",
-                                "fr-grid-row--gutters"
-                            )}
-                        >
-                            <div className={fr.cx("fr-col-3")}>
-                                <Input
-                                    label="Date de début"
-                                    hintText="Début de ta mission"
-                                    nativeInputProps={{
-                                        style: { width: 200 },
-                                        placeholder: "JJ/MM/YYYY",
-                                        type: "date",
-                                        ...register(`missions.${index}.start`),
-                                    }}
-                                    {...defaultState("start")}
-                                />
-                            </div>{" "}
-                            <div className={fr.cx("fr-col-4")}>
-                                <Input
-                                    label="Date de fin"
-                                    nativeInputProps={{
-                                        style: { width: 200 },
-                                        placeholder: "JJ/MM/YYYY",
-                                        type: "date",
-                                        ...register(`missions.${index}.end`),
-                                    }}
-                                    hintText={
-                                        <div>
-                                            En cas de doute, mettre{" "}
-                                            <button
-                                                className={fr.cx(
-                                                    "fr-link",
-                                                    "fr-text--xs"
-                                                )}
-                                                onClick={() =>
-                                                    onMissionAutoEndClick(index)
-                                                }
-                                                role="button"
-                                                type="button"
-                                                title="Mettre la date de fin à +3 mois"
-                                            >
-                                                J+3 mois
-                                            </button>
-                                        </div>
-                                    }
-                                    {...defaultState("end")}
-                                />
-                            </div>
-                        </div>
-
-                        <div
-                            className={fr.cx(
-                                "fr-grid-row",
-                                "fr-grid-row--gutters"
-                            )}
-                        >
-                            <div className={fr.cx("fr-col-6")}>
-                                <Input
-                                    label="Employeur"
-                                    nativeInputProps={{
-                                        placeholder: "ex: Scopyleft",
-                                        ...register(
-                                            `missions.${index}.employer`
-                                        ),
-                                    }}
-                                    {...defaultState("employer")}
-                                />
-                            </div>
-                            <div className={fr.cx("fr-col-6")}>
-                                <Select
-                                    label="Statut"
-                                    nativeSelectProps={{
-                                        ...register(`missions.${index}.status`),
-                                    }}
-                                    {...defaultState("status")}
-                                >
-                                    <option value="">Statut:</option>
-                                    {userStatusOptions.map((option) => (
-                                        <option
-                                            key={option.key}
-                                            selected={
-                                                option.key === mission.status
-                                            }
-                                            value={option.key}
-                                        >
-                                            {option.name}
-                                        </option>
-                                    ))}
-                                </Select>{" "}
-                            </div>
-                        </div>
-                        <div
-                            className={fr.cx(
-                                "fr-grid-row",
-                                "fr-grid-row--gutters"
-                            )}
-                        >
-                            <div className={fr.cx("fr-col-12")}>
-                                <StartupsPicker
-                                    name="startups"
-                                    control={control}
-                                    startups={startupOptions}
-                                    label="Produits concernés par la mission :"
-                                    defaultValue={startupOptions.filter(
-                                        (s) =>
-                                            mission.startups &&
-                                            mission.startups.includes(s.value)
-                                    )}
-                                    onChange={(startups) => {
-                                        console.log("startups", startups);
-                                        setValue(
-                                            `missions.${index}.startups`,
-                                            startups.map(
-                                                (startup) => startup.value
-                                            ),
-                                            {
-                                                shouldValidate: true,
-                                                shouldDirty: true,
-                                            }
-                                        );
-                                    }}
-                                    {...defaultState("startups")}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
+            {missionsFields.map((mission, index, all) => (
+                <Mission
+                    key={mission.id}
+                    index={index}
+                    mission={mission}
+                    control={control}
+                    register={register}
+                    setValue={setValue}
+                    missionsRemove={() => missionsRemove(index)}
+                    onMissionAutoEndClick={() => onMissionAutoEndClick(index)}
+                    startupOptions={startupOptions}
+                    errors={errors ? errors[index] : undefined}
+                />
+            ))}
             <Button
                 iconId="fr-icon-add-circle-line"
                 priority="secondary"
