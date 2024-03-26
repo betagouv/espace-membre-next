3197c3698683a43b831b96e147a977d3f9388ba9
Lucas Charrier <lucharrier@gmail.com>
2024-03-26 17:58:31 +0100

    chore: add live chat component

---
+++ src/components/live-chat/LiveChatContext.tsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export interface LiveChatContextType {
+    showLiveChat: (type?: string) => void;
+    isLiveChatLoading: boolean;
+}
+
+export const LiveChatContext = createContext<LiveChatContextType>({
+    showLiveChat: (type?: string) => {
+        throw new Error("the LiveChatProvider is missing");
+    },
+    isLiveChatLoading: false,
+});
