90f573d7a79201f3deac68bdc404f03da00e4e71
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-06-12 19:09:44 +0200

    fix

--- src/scripts/import-from-www.ts
+++ src/scripts/import-from-www.ts
@@ -5,76 +5,72 @@ import { db } from "@lib/kysely";
 import { startup, author, organisation, incubator } from "./github-schemas";
 import { ZodSchema, z } from "zod";
 import pAll from "p-all";
-import { importFromZip } from "./utils";
+import { importFromZip, MarkdownData } from "./utils";
 
-const insertData = async (markdownData) => {
+const insertData = async (markdownData: MarkdownData) => {
     // truncate tables
     // do not drop users -- await db.deleteFrom("users").execute();
     await db.deleteFrom("missions_startups").execute();
     await db.deleteFrom("missions").execute();
     await db.deleteFrom("phases").execute();
     await db.deleteFrom("startups_organizations").execute();
+    await db.deleteFrom("startup_events").execute();
     await db.deleteFrom("startups").execute();
     await db.deleteFrom("incubators").execute();
     await db.deleteFrom("organizations").execute();
 
     // insert organisations
-    const organisations = await pAll(
-        [
-            // () =>
-            //     db
-            //         .insertInto("organizations")
-            //         .values({
-            //             name: "test",
-            //             ghid: "test",
-            //             type: "autre",
-            //             domaine_ministeriel: "",
-            //         })
-            //         .returning(["uuid", "ghid"])
-            //         .executeTakeFirstOrThrow(),
-            ...markdownData.organisations.map((orga) => async () => {
-                const query = db
-                    .insertInto("organizations")
-                    .values({
-                        domaine_ministeriel:
-                            orga.attributes.domaine_ministeriel || "",
-                        name: orga.attributes.name,
-                        acronym: orga.attributes.acronym,
-                        type: orga.attributes.type,
-                        ghid: orga.attributes.ghid,
-                    })
-                    .returning(["uuid", "ghid"]);
-                return query.executeTakeFirstOrThrow();
-            }),
-        ],
-        { concurrency: 1 }
-    );
+    const organisations = await db
+        .insertInto("organizations")
+        .values(
+            markdownData.organisations.map((orga) => ({
+                domaine_ministeriel: orga.attributes.domaine_ministeriel || "",
+                name: orga.attributes.name,
+                acronym: orga.attributes.acronym,
+                type: orga.attributes.type,
+                ghid: orga.attributes.ghid,
+            }))
+        )
+        .returning(["uuid", "ghid"])
+        .execute();
 
     // insert incubators
-    const incubators = await pAll(
-        markdownData.incubators.map((incub) => async () => {
-            const owner_id = organisations.find(
-                (o) =>
-                    o.ghid ===
-                    (incub.attributes.owner &&
-                        incub.attributes.owner.replace("/organisations/", ""))
-            )?.uuid;
-            const query = db
-                .insertInto("incubators")
-                .values({
+    const incubators = await db
+        .insertInto("incubators")
+        .values(({ selectFrom }) =>
+            markdownData.incubators
+                .filter((incub) => {
+                    if (!incub.attributes.owner) {
+                        console.error(
+                            `IncubatorError: invalid owner for ${incub.attributes.ghid}`
+                        );
+                        return false;
+                    }
+                    return true;
+                })
+                .map((incub) => ({
                     title: incub.attributes.title,
                     address: incub.attributes.address,
                     contact: incub.attributes.contact,
                     github: incub.attributes.github,
-                    owner_id,
+                    owner_id: selectFrom("organizations")
+                        .where(
+                            "ghid",
+                            "=",
+                            incub.attributes.owner.replace(
+                                "/organisations/",
+                                ""
+                            )
+                        )
+                        .select("uuid"),
                     website: incub.attributes.website,
                     ghid: incub.attributes.ghid,
-                })
-                .returning(["uuid", "ghid"]);
-            return query.executeTakeFirstOrThrow();
-        }),
-        { concurrency: 1 }
-    );
+                    description: incub.body,
+                    short_description: incub.attributes.short_description,
+                }))
+        )
+        .returning(["uuid", "ghid"])
+        .execute();
 
     // insert startups
     const startups = await pAll(
@@ -89,9 +85,7 @@ const insertData = async (markdownData) => {
                     contact: startup.attributes.contact,
                     incubator_id,
                     link: startup.attributes.link,
-                    website: startup.attributes.link, // ??
                     repository: startup.attributes.repository,
-                    github: startup.attributes.repository, // ??
                     accessibility_status:
                         startup.attributes.accessibility_status,
                     analyse_risques: startup.attributes.analyse_risques,
@@ -107,11 +101,12 @@ const insertData = async (markdownData) => {
                     usertypes: JSON.stringify(startup.attributes.usertypes),
                     techno: JSON.stringify(startup.attributes.techno),
                     description: startup.body,
-                    id: startup.attributes.ghid,
+                    ghid: startup.attributes.ghid,
                 })
-                .returning(["uuid", "id"]);
+                .returning(["uuid", "ghid"]);
 
             const startupDb = await query.executeTakeFirstOrThrow();
+
             // phases
             const phaseNames =
                 startup.attributes.phases?.map((p) => p.name) || [];
@@ -152,40 +147,55 @@ const insertData = async (markdownData) => {
                 { concurrency: 1 }
             );
 
-            // sponsors
-            await pAll(
-                startup.attributes.sponsors?.map((sponsor) => () => {
-                    let organization_id = sponsor.replace(
-                        "/organisations/",
-                        ""
-                    );
-                    // if (
-                    //     sponsor === "/organisations/ofpra" ||
-                    //     sponsor === "/organisations/csm" ||
-                    //     sponsor === "/organisations/idfm"
-                    // ) {
-                    //     // todo: drop buggy files
-                    //     console.error(
-                    //         `SponsorError: unknown sponsor ${sponsor} in https://github.com/betagouv/beta.gouv.fr/blob/master/content/_startups/${startup.attributes.ghid}.md`
-                    //     );
-                    //     // organization_id = "test";
-                    // }
-                    return db
-                        .insertInto("startups_organizations")
-                        .values(({ selectFrom }) => ({
-                            organization_id: selectFrom("organizations")
-                                .where("ghid", "=", organization_id)
-                                .select("uuid"),
-                            startup_id: startupDb.uuid,
-                        }))
-                        .execute();
-                }) || [],
-                { concurrency: 1 }
-            );
+            if (
+                startup.attributes.sponsors &&
+                startup.attributes.sponsors.length
+            ) {
+                await db
+                    .insertInto("startups_organizations")
+                    .values(
+                        ({ selectFrom }) =>
+                            startup.attributes.sponsors?.map((sponsor) => ({
+                                organization_id: selectFrom("organizations")
+                                    .where(
+                                        "ghid",
+                                        "=",
+                                        sponsor.replace("/organisations/", "")
+                                    )
+                                    .select("uuid"),
+                                startup_id: startupDb.uuid,
+                            })) || []
+                    )
+                    .execute();
+            }
 
             // events
-            // fast
-            // todo
+            if (startup.attributes.events && startup.attributes.events.length) {
+                await db
+                    .insertInto("startup_events")
+                    .values(
+                        startup.attributes.events?.map((event) => ({
+                            name: event.name,
+                            date: event.date,
+                            comment: event.comment,
+                            startup_id: startupDb.uuid,
+                        })) || []
+                    )
+                    .execute();
+            }
+
+            if (startup.attributes.fast) {
+                await db
+                    .insertInto("startup_events")
+                    .values({
+                        name: "fast",
+                        date: `2024-01-01`,
+                        comment: `Montant de ${startup.attributes.fast?.montant}€ pour la promotion ${startup.attributes.fast?.promotion}`,
+                        startup_id: startupDb.uuid,
+                    })
+                    .execute();
+            }
+
             return startupDb;
         }),
         { concurrency: 1 }
@@ -256,9 +266,9 @@ const insertData = async (markdownData) => {
                             .insertInto("missions_startups")
                             .values(
                                 ({ selectFrom }) =>
-                                    mission.startups?.map((s) => ({
+                                    mission.startups?.map((startup) => ({
                                         startup_id: selectFrom("startups")
-                                            .where("id", "=", s)
+                                            .where("ghid", "=", startup)
                                             .select("uuid"),
                                         mission_id,
                                     })) || []
