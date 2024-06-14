be94ac3ba350d07c29638baa24bde7021688972b
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-06-14 09:27:11 +0200

    fix

--- e2e/auth.valid.member.setup.ts
+++ e2e/auth.valid.member.setup.ts
@@ -2,7 +2,7 @@ import { test as setup, expect } from "@playwright/test";
 
 const adminFile = "./playwright-auth-valid.member.json";
 
-setup("authenticate as valid.member", async ({ page }) => {
+setup("authenticate as valid.member through magic link", async ({ page }) => {
     // fill login form
     await page.goto("/");
     await page.getByLabel("Mon email").fill("valid.member@betagouv.ovh");
@@ -11,14 +11,14 @@ setup("authenticate as valid.member", async ({ page }) => {
     // wait a while to get the latest email
     await page.waitForTimeout(1000);
 
-    // open maildev and click first invitation link
+    // open maildev and click the first invitation link
     await page.goto("http://127.0.0.1:1080");
     await page
         .getByText("Connexion à l'espace membre BetaGouv")
         .first()
         .click();
 
-    // get the magic link from the email
+    // get the magic link from the email preview
     const iframe = await page.frameLocator(".preview-iframe").first();
     await iframe.getByText("Me connecter", { exact: true }).click();
     const href =
