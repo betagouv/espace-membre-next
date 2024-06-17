a8d43f3dc04c02f1cc72de716e1c59863095c4f4
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-06-17 09:20:11 +0200

    fix

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -62,15 +62,15 @@ export default defineConfig({
         //   },
         // },
         // Test against mobile viewports.
-        {
-            name: "Mobile Chrome",
-            use: {
-                ...devices["Pixel 5"],
-            },
-        },
-        {
-            name: "Mobile Safari",
-            use: devices["iPhone 12"],
-        },
+        // {
+        //     name: "Mobile Chrome",
+        //     use: {
+        //         ...devices["Pixel 5"],
+        //     },
+        // },
+        // {
+        //     name: "Mobile Safari",
+        //     use: devices["iPhone 12"],
+        // },
     ],
 });
