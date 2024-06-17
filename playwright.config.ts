3f79648110fc4519816bf25fe7f47382b15007b2
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-06-17 08:47:20 +0200

    timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,8 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
     },
-
+    timeout: 60000,
+    expect: { timeout: 20000 },
     /* Configure projects for major browsers */
     projects: [
         { name: "setup", testMatch: /.*\.setup\.ts/ },
