c5369afcff9844d96d3f608465ebc0f4942f9fd0
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-06-26 02:09:47 +0200

    fix

--- src/lib/kysely/queries/users.ts
+++ src/lib/kysely/queries/users.ts
@@ -1,18 +1,9 @@
-import {
-    sql,
-    ExpressionBuilder,
-    Kysely,
-    SelectQueryBuilder,
-    SelectExpression,
-    ExpressionWrapper,
-    Expression,
-} from "kysely";
+import { sql, ExpressionBuilder, Kysely, SelectExpression } from "kysely";
 import { UpdateObjectExpression } from "kysely/dist/cjs/parser/update-set-parser";
-
 import { DB } from "@/@types/db"; // generated with `npm run kysely-codegen`
 import { db as database, jsonArrayFrom } from "@/lib/kysely";
 
-const MEMBER_PROTECTED_INFO: SelectExpression<DB, "users">[] = [
+export const MEMBER_PROTECTED_INFO: SelectExpression<DB, "users">[] = [
     "users.uuid",
     "users.updated_at",
     "users.username",
@@ -135,130 +126,6 @@ export async function getAllUsersInfo(db: Kysely<DB> = database) {
         .select((eb) => [...MEMBER_PROTECTED_INFO, withMissions(eb)])
         .compile();
 
-    const userInfos = await db.executeQuery(query);
-
-    return userInfos.rows;
-}
-
-// get list of startups with latest phase
-const getSesWithLatestPhaseQuery = (db: Kysely<DB> = database) =>
-    db
-        .with("latest_phases", (db) =>
-            // compute SES latest phase
-            db
-                .selectFrom("phases")
-                .select((eb) => [
-                    "startup_id",
-                    "name",
-                    "start",
-                    "end",
-                    sql<number>`ROW_NUMBER() OVER (PARTITION BY startup_id ORDER BY "end" DESC NULLS FIRST, start desc)`.as(
-                        "rn"
-                    ),
-                ])
-        )
-        .selectFrom("latest_phases")
-        .where("latest_phases.rn", "=", 1)
-        .orderBy("start", "desc");
-
-// search and get users public info
-export async function searchUsers(
-    filters: URLSearchParams,
-    db: Kysely<DB> = database
-) {
-    // todo
-    const startupPhases =
-        filters.get("startupPhases")?.split(",").filter(Boolean) || [];
-
-    const validStartups: string[] = [];
-    if (startupPhases.length) {
-        // select appropriate SES before-hand
-        const ses = await getSesWithLatestPhaseQuery()
-            .select("startup_id")
-            .where("latest_phases.name", "in", startupPhases)
-            .execute();
-        validStartups.push(...ses.map((s) => s.startup_id));
-    }
-
-    const query = db
-        .with("users_expiry", (db) => {
-            return db
-                .selectFrom("missions")
-                .select((eb) => [
-                    "user_id",
-                    sql<boolean>`MAX(COALESCE(missions.end, '2200-02-02'))<=NOW()`.as(
-                        "expired"
-                    ),
-                ])
-                .groupBy("missions.user_id");
-        })
-        .selectFrom("users_expiry")
-        .leftJoin("users", "users.uuid", "users_expiry.user_id")
-        .leftJoin("missions", "missions.user_id", "users.uuid")
-        .leftJoin(
-            "missions_startups",
-            "missions_startups.mission_id",
-            "missions.uuid"
-        )
-        .leftJoin("startups", "startups.uuid", "missions_startups.startup_id")
-        .select(({ eb, selectFrom }) => [
-            ...MEMBER_PROTECTED_INFO, // aggregate startups names
-            sql<
-                Array<string>
-            >`array_agg(startups.name order by startups.name)`.as("startups"),
-        ])
-        .where(({ eb, ref }) => {
-            const conditions: Expression<any>[] = [];
-            const domaines =
-                filters.get("domaines")?.split(",").filter(Boolean) || [];
-            const incubators =
-                filters.get("incubators")?.split(",").filter(Boolean) || [];
-
-            const memberStatus =
-                filters.get("memberStatus")?.split(",").filter(Boolean) || [];
-            const startups =
-                filters.get("startups")?.split(",").filter(Boolean) || [];
-            const competences =
-                filters.get("competences")?.split(",").filter(Boolean) || [];
-            if (domaines.length) {
-                conditions.push(eb("domaine", "in", domaines));
-            }
-            if (incubators.length) {
-                conditions.push(eb("startups.incubator_id", "in", incubators));
-            }
-            if (startups.length) {
-                conditions.push(eb("startups.uuid", "in", startups));
-            }
-            if (competences.length) {
-                // https://www.postgresql.org/docs/9.5/functions-json.html
-                conditions.push(
-                    eb(ref("competences"), "?&", eb.val(competences))
-                );
-            }
-            if (validStartups.length) {
-                conditions.push(eb("startups.uuid", "in", validStartups));
-            }
-
-            if (memberStatus.length) {
-                const expiredStatus: boolean[] = [];
-                if (memberStatus.includes("active")) {
-                    expiredStatus.push(false);
-                }
-                if (memberStatus.includes("unactive")) {
-                    expiredStatus.push(true);
-                }
-                conditions.push(
-                    eb.or(expiredStatus.map((s) => eb("expired", "is", s)))
-                );
-            }
-
-            return eb.and(conditions);
-        })
-        .groupBy([...MEMBER_PROTECTED_INFO])
-        .orderBy(["users.fullname"])
-        .compile();
-
-    //console.log(query.sql);
     const userInfos = await db.executeQuery(query);
 
     return userInfos.rows;
