99593d9f50f4bc7c3f49b0c8c271d71a66618f8d
Lucas Charrier <lucharrier@gmail.com>
2024-07-02 10:10:19 +0200

    fix: endpoint is authorized only for logged users

--- src/app/api/members/route.ts
+++ src/app/api/members/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest } from "next/server";
+import { getServerSession } from "next-auth/next";
+
 import { searchUsers } from "@/lib/kysely/queries/search";
 
 export async function GET(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user.id) {
+        throw new Error(`You don't have the right to access this function`);
+    }
     const { searchParams } = new URL(req.url);
     const users = await searchUsers(searchParams);
     const data = { users };
