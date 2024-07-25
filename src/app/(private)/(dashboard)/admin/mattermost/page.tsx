8b1eeff89caa7c7d472d385f049f5006bd3f5514
Lucas Charrier <lucharrier@gmail.com>
2024-07-25 17:15:54 +0200

    chore: add/fix page title for team pages

--- src/app/(private)/(dashboard)/admin/mattermost/page.tsx
+++ src/app/(private)/(dashboard)/admin/mattermost/page.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+
 import AdminMattermostClientPage from "./AdminMattermostClientPage";
 import { routeTitles } from "@/utils/routes/routeTitles";
 
 export const metadata: Metadata = {
-    title: `${routeTitles.accountEditPrivateInfo()} / Espace Membre`,
+    title: `${routeTitles.adminMattermost()} / Espace Membre`,
 };
 
 export default function Page(props) {
