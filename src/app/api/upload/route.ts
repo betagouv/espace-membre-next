379fb536fffef230155e9099bd97ab61bdfb8d1e
Lucas Charrier <lucharrier@gmail.com>
2024-07-30 16:31:20 +0200

    feat: add s3 upload endpoint, and image display endpoint

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,55 @@
+// pages/api/upload.ts
+import AWS from "aws-sdk";
+import { NextRequest } from "next/server";
+import { getServerSession } from "next-auth/next";
+
+import config from "@/server/config";
+import { authOptions } from "@/utils/authoptions";
+
+// Configure AWS SDK
+const s3 = new AWS.S3({
+    accessKeyId: config.S3_KEY_ID,
+    secretAccessKey: config.S3_KEY_SECRET,
+    region: "US",
+    endpoint: new AWS.Endpoint(config.S3_HOST!),
+    s3ForcePathStyle: true, // Needed for some S3-compatible storage services
+});
+
+const getFileName = {
+    member: (username) => `members/${username}/avatar.jpg`,
+    startupHero: (startupId) => `s3/startups/${startupId}/hero.jpg`,
+    startupShot: (startupId) => `s3/startups/${startupId}/shot.jpg`,
+};
+
+export async function POST(req: NextRequest) {
+    const { fileName, fileType } = await req.json();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || session.user.id !== fileName) {
+        throw new Error(`You don't have the right to access this function`);
+    }
+
+    const s3Params = {
+        Bucket: config.S3_BUCKET,
+        Key: getFileName["member"](fileName),
+        Expires: 60,
+        ContentType: fileType,
+    };
+
+    try {
+        const signedUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+
+        return Response.json({ signedUrl }, { status: 200 });
+    } catch (error) {
+        console.log("LCS ERRORS");
+        return Response.json(
+            {
+                error: "Failed to generate signed URL",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
