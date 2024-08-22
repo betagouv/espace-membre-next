6ba44ebb89427e3f40c31399e6cbce90c1523688
Lucas Charrier <lucharrier@gmail.com>
2024-08-22 09:38:39 +0200

    chore: add file admin action

---
+++ src/app/api/admin/action.ts
@@ -0,0 +1,16 @@
+"use server";
+import { getServerSession } from "next-auth/next";
+
+import { smtpBlockedContactsEmailDelete } from "@/server/config/email.config";
+import { authOptions } from "@/utils/authoptions";
+import { AuthorizationError } from "@/utils/error";
+
+export async function unblockMemberEmailAddress(email: string) {
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user.id || !session.user.isAdmin) {
+        throw new AuthorizationError();
+    }
+    smtpBlockedContactsEmailDelete({
+        email,
+    });
+}
