bebba862161852a40c984611a66e8af14f828bc8
Lucas Charrier <lucharrier@gmail.com>
2024-09-11 18:46:57 +0200

    chore: authorize s3 url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 const cspHeader = `
     default-src 'self';
-    connect-src 'self' api.maptiler.com *.gouv.fr sentry.incubateur.net;
+    connect-src 'self' api.maptiler.com *.gouv.fr sentry.incubateur.net espace-membre.cellar-c2.services.clever-cloud.com;
     script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: *.gouv.fr sentry.incubateur.net unpkg.com;
     style-src 'self' 'unsafe-inline' cdnjs.cloudflare.com unpkg.com;
     img-src * data: blob:;
