b16d2afa00c1be321918ea2139f4c4f2dd528c3e
Lucas Charrier <lucharrier@gmail.com>
2024-09-11 13:11:37 +0200

    fix: add delete image or save image when boolean set to true

--- src/components/IncubatorCreatePage/IncubatorCreate.tsx
+++ src/components/IncubatorCreatePage/IncubatorCreate.tsx
@@ -6,7 +6,9 @@ import * as Sentry from "@sentry/nextjs";
 
 import { IncubatorForm } from "../IncubatorForm/IncubatorForm";
 import { createIncubator } from "@/app/api/incubators/actions/createIncubator";
+import { incubatorUpdateSchemaType } from "@/models/actions/incubator";
 import { Option } from "@/models/misc";
+import { saveImage } from "@/utils/file";
 
 interface IncubatorInfoCreateProps {
     sponsorOptions: Option[];
@@ -15,19 +17,39 @@ interface IncubatorInfoCreateProps {
 
 /* Pure component */
 export const IncubatorCreate = (props: IncubatorInfoCreateProps) => {
-    const save = async (data) => {
-        await createIncubator({
-            incubator: data,
-        })
-            .then((result) => {
-                window.scrollTo({ top: 20, behavior: "smooth" });
-                return result;
-            })
-            .catch((e) => {
-                window.scrollTo({ top: 20, behavior: "smooth" });
-                Sentry.captureException(e);
-                throw e;
+    const save = async (data: incubatorUpdateSchemaType) => {
+        try {
+            const newIncubator = await safeCreateIncubator({
+                incubator: data,
             });
+            if (data.logo) {
+                saveImage({
+                    fileIdentifier: "logo",
+                    fileRelativeObjType: "incubator",
+                    fileObjIdentifier: newIncubator.ghid,
+                    file: data.logo,
+                });
+            }
+
+            if (data.shouldDeleteLogo) {
+                await fetch("/api/image", {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        fileObjIdentifier: newIncubator.ghid,
+                        fileIdentifer: "hero",
+                        fileRelativeObjType: "startup",
+                    }),
+                });
+            }
+            window.scrollTo({ top: 20, behavior: "smooth" });
+        } catch (e) {
+            window.scrollTo({ top: 20, behavior: "smooth" });
+            Sentry.captureException(e);
+            throw e;
+        }
     };
     return (
         <>
