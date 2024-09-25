63c4c6fb69fadfa211451ec5e0841a54db220453
Charrier <lucharrier@gmail.com>
2024-09-25 12:36:18 +0200

    chore: add env var configurable alert message on private and public pages (#524)

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,9 +2,26 @@
 
 import { PropsWithChildren } from "react";
 
+import Alert from "@codegouvfr/react-dsfr/Alert";
+
+import frontConfig from "@/frontConfig";
+
 export default function Layout(props: PropsWithChildren) {
     return (
         <div className="fr-grid-row fr-grid-row-gutters fr-grid-row--center ">
+            {!!frontConfig.NEXT_PUBLIC_ALERT_MESSAGE_PUBLIC && (
+                <Alert
+                    className="fr-mb-4v fr-mt-4v"
+                    severity={
+                        frontConfig.NEXT_PUBLIC_ALERT_MESSAGE_PUBLIC.severity
+                    }
+                    closable={false}
+                    description={
+                        frontConfig.NEXT_PUBLIC_ALERT_MESSAGE_PUBLIC.description
+                    }
+                    title={frontConfig.NEXT_PUBLIC_ALERT_MESSAGE_PUBLIC.title}
+                />
+            )}
             {props.children}
         </div>
     );
