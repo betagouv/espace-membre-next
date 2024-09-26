f77112b83dec66e21f6f770e2e74eafdbfab451a
Lucas Charrier <lucharrier@gmail.com>
2024-09-26 11:02:28 +0200

    chore: add signaux faibles beta page

---
+++ src/app/(private)/(dashboard)/admin/signaux-faibles-beta/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+
+import { AdminProductClientPage } from "./AdminProductClientPage";
+import { buildStartupDashboardData } from "@/server/schedulers/buildStartupDashboardData";
+import { authOptions } from "@/utils/authoptions";
+import { routeTitles } from "@/utils/routes/routeTitles";
+
+export const metadata: Metadata = {
+    title: `${routeTitles.adminMattermost()} / Espace Membre`,
+};
+
+export default async function Page(props) {
+    const session = await getServerSession(authOptions);
+
+    if (session && !session.user.isAdmin) {
+        redirect("/dashboard");
+    }
+    const data = await buildStartupDashboardData();
+    return <AdminProductClientPage data={data} title={""} />;
+}
