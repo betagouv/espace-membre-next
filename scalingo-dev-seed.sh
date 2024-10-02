000057a42770c4e46edc0e844e585d7f2f177be8
Lucas Charrier <lucharrier@gmail.com>
2024-10-02 15:09:09 +0200

    chore: run import from www on first deploy

--- scalingo-dev-seed.sh
+++ scalingo-dev-seed.sh
@@ -4,6 +4,7 @@ if [ "$SEED_FAKE_DATA_DATABASE" = "true" ] ; then
   echo "Seeding database";
   npm run migrate;
   npm run seed;
+  npm run import-from-www;
 else
   echo "Not seeding database";
 fi
