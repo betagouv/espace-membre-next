dab1128af20fa3013ef8323d18cb0f9889f5ba2d
Lucas Charrier <lucharrier@gmail.com>
2024-10-02 15:12:43 +0200

    chore: add import-from-www to seeding

--- scalingo-dev-seed.sh
+++ scalingo-dev-seed.sh
@@ -4,7 +4,7 @@ if [ "$SEED_FAKE_DATA_DATABASE" = "true" ] ; then
   echo "Seeding database";
   npm run migrate;
   npm run seed;
-  npm run import-from-www;
+  npm run staging-import-from-www;
 else
   echo "Not seeding database";
 fi
