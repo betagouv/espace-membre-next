d032c0765401841d38264aafa2fcb3fa676a539d
Charrier <lucharrier@gmail.com>
2024-10-02 09:03:04 +0200

    chore: update check to avoid open redirect to url //evil.com or \/evi… (#517)
    * chore: update check to avoid open redirect to url //evil.com or \/evil.com
    
    * test: add test with redirect url change to have format //evil.com and \/evil.com
    
    * test: changed query parameter to modified callbackUrl instead of callback
    
    * test: fix changing callbackUrl that appear in the url after anchor parameter
    
    * test: changed url to test
    
    * fix: replace app.json by scalingo.json add HOST_CANONICAL_URL
    
    * chore: always preprend hostname
    
    * chore: make always use origin works for absolute url
    
    * fix: nativateToPage get as argument the loginURL and not the callbackUrl
    
    * chore: changed redirection url in try catch

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -127,3 +127,119 @@ test("valid login sends magic link and redirect to the page pass in next searchP
     // Verify the URL is correct after redirection
     expect(pathname).toBe("/community/valid.member");
 });
+
+// test with redirect to //evil.com
+test("valid login sends magic link and but the callback url is changed to be evil website (//evil.com), should be ignore and redirect to /dashboard", async ({
+    page,
+    browser,
+}) => {
+    await page.goto("/login?next=/community/valid.member");
+    await page.getByLabel("Mon email").fill("valid.member@betagouv.ovh");
+    await page.getByText("Recevoir le lien de connexion").click();
+    await page.waitForTimeout(2000);
+
+    await expect(
+        page.getByText(
+            "Un email avec un lien de connexion a été envoyé à ton adresse."
+        )
+    ).toBeVisible();
+
+    // maildev
+    await page.goto("http://127.0.0.1:1080");
+
+    await page
+        .getByText("Connexion à l'espace membre BetaGouv")
+        .first()
+        .click();
+    await page.waitForTimeout(2000);
+
+    const iframe = await page.frameLocator(".preview-iframe").first();
+
+    // Get the original magic link (href) from the email
+    const originalHref = await iframe
+        .getByText("Me connecter", { exact: true })
+        .getAttribute("href");
+
+    // Ensure that we have a valid href
+    if (!originalHref) {
+        throw new Error("Magic link not found in the email.");
+    }
+
+    // Parse the URL and fragment (the part after the #)
+    const [baseUrl, fragment] = originalHref.split("#");
+
+    // Parse the fragment as a URL
+    const fragmentUrl = new URL(fragment);
+
+    // Modify the callbackUrl parameter
+    fragmentUrl.searchParams.set("callbackUrl", "//evil.com");
+
+    // Rebuild the full URL with the modified callbackUrl
+    const modifiedHref = `${baseUrl}#${fragmentUrl.toString()}`;
+    // Navigate to the modified URL
+    await page.goto(modifiedHref);
+    await page.getByText("Me connecter").first().click();
+    await page.waitForURL("/dashboard");
+    const url = new URL(page.url());
+    const pathname = url.pathname;
+    // Verify the URL is correct after redirection
+    expect(pathname).toBe("/dashboard");
+});
+
+// test with redirect to /\evil.com
+test(`valid login sends magic link and but the callback url is changed to be evil website (/\evil.com), should be ignore and redirect to /dashboard`, async ({
+    page,
+    browser,
+}) => {
+    await page.goto("/login?next=/community/valid.member");
+    await page.getByLabel("Mon email").fill("valid.member@betagouv.ovh");
+    await page.getByText("Recevoir le lien de connexion").click();
+    await page.waitForTimeout(2000);
+
+    await expect(
+        page.getByText(
+            "Un email avec un lien de connexion a été envoyé à ton adresse."
+        )
+    ).toBeVisible();
+
+    // maildev
+    await page.goto("http://127.0.0.1:1080");
+
+    await page
+        .getByText("Connexion à l'espace membre BetaGouv")
+        .first()
+        .click();
+    await page.waitForTimeout(2000);
+
+    const iframe = await page.frameLocator(".preview-iframe").first();
+
+    // Get the original magic link (href) from the email
+    const originalHref = await iframe
+        .getByText("Me connecter", { exact: true })
+        .getAttribute("href");
+
+    // Ensure that we have a valid href
+    if (!originalHref) {
+        throw new Error("Magic link not found in the email.");
+    }
+
+    // Parse the URL and fragment (the part after the #)
+    const [baseUrl, fragment] = originalHref.split("#");
+
+    // Parse the fragment as a URL
+    const fragmentUrl = new URL(fragment);
+
+    // Modify the callbackUrl parameter
+    fragmentUrl.searchParams.set("callbackUrl", `/\\evil.com`);
+
+    // Rebuild the full URL with the modified callbackUrl
+    const modifiedHref = `${baseUrl}#${fragmentUrl.toString()}`;
+    // Navigate to the modified URL
+    await page.goto(modifiedHref);
+    await page.getByText("Me connecter").first().click();
+    await page.waitForURL("/dashboard");
+    const url = new URL(page.url());
+    const pathname = url.pathname;
+    // Verify the URL is correct after redirection
+    expect(pathname).toBe("/dashboard");
+});
