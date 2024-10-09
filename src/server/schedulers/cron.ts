7ca7a1f0309e5db6368eb670537079ea1acde01e
Charrier <lucharrier@gmail.com>
2024-10-09 18:37:44 +0200

    Update cron.ts (#559)

--- src/server/schedulers/cron.ts
+++ src/server/schedulers/cron.ts
@@ -222,7 +222,7 @@ const startupJobs: Job[] = [
 
 const servicesJobs: Job[] = [
     {
-        cronTime: "0 0 15 * * *",
+        cronTime: "0 45 18 * * *",
         onTick: deleteMatomoAccount,
         isActive: !!config.FEATURE_DELETE_MATOMO_ACCOUNT,
         name: "deleteMatomoAccount",
