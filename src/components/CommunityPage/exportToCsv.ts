287e63aac22dfbdf2128805bd48114cb20d9e68f
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-10-13 07:53:06 +0200

    feat(search): revamp

---
+++ src/components/CommunityPage/exportToCsv.ts
@@ -0,0 +1,31 @@
+export function exportToCsv(filename, rows) {
+    const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
+    const header = Object.keys(rows[0]);
+    const csv = [
+        header.join(";"), // header row first
+        ...rows.map((row) =>
+            header
+                .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+                .join(";")
+        ),
+    ].join("\r\n");
+
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    if (navigator["msSaveBlob"]) {
+        // IE 10+
+        navigator["msSaveBlob"](blob, filename);
+    } else {
+        const link = document.createElement("a");
+        if (link.download !== undefined) {
+            // feature detection
+            // Browsers that support HTML5 download attribute
+            const url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = "hidden";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
--- src/lib/kysely/queries/incubators.ts
+++ src/lib/kysely/queries/incubators.ts
@@ -25,3 +25,41 @@ export async function getIncubator(uuid: string) {
         .where("uuid", "=", uuid)
         .executeTakeFirst();
 }
+
+export async function getAllIncubatorsMembers() {
+    const incubs = await getAllIncubators();
+    return Promise.all(
+        incubs.map(async (incub) => {
+            const teamMembers = await db
+                .selectFrom("users")
+                .select(["users.uuid", "users.fullname"])
+                .leftJoin("users_teams", "users_teams.user_id", "users.uuid")
+                .leftJoin("teams", "teams.uuid", "users_teams.team_id")
+                .leftJoin("incubators", "teams.incubator_id", "incubators.uuid")
+                .where("teams.incubator_id", "=", incub.uuid)
+                .execute();
+            const startupMembers = await db
+                .selectFrom("users")
+                .select(["users.uuid", "users.fullname"])
+                .leftJoin("missions", "missions.user_id", "users.uuid")
+                .leftJoin(
+                    "missions_startups",
+                    "missions_startups.mission_id",
+                    "missions.uuid"
+                )
+                .leftJoin(
+                    "startups",
+                    "startups.uuid",
+                    "missions_startups.startup_id"
+                )
+                .where("startups.incubator_id", "=", incub.uuid)
+                .execute();
+            return {
+                // ...incub,
+                id: incub.uuid,
+                title: incub.title,
+                members: [...teamMembers, ...startupMembers],
+            };
+        })
+    );
+}
