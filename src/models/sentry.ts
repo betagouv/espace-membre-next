321bfac8ee866603377a74548a37ee69a7f48a7e
Lucas Charrier <lucharrier@gmail.com>
2024-10-17 19:57:17 +0200

    chore: add sentry relative files

---
+++ src/models/sentry.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+const sentryUserMetadataSchema = z.object({
+    organisationRole: z.enum(["admin", "member", "manager", "owner"]),
+    pending: z.boolean(),
+    expired: z.boolean(),
+    inviteStatus: z.enum(["approved", "pending"]),
+    teams: z.array(
+        z.object({
+            role: z.enum(["admin", "contributor"]).nullable(), // role is null if users has highest privelege at organization level
+            id: z.string(),
+            slug: z.string(),
+            name: z.string(),
+            memberCount: z.number(),
+            projects: z.array(
+                z.object({
+                    id: z.string(),
+                    slug: z.string(),
+                    name: z.string(),
+                    plateform: z.string(),
+                })
+            ),
+        })
+    ),
+});
+
+export const sentryUserSchema = z.object({
+    email: z.string(),
+    account_type: z.literal("sentry"),
+    service_user_id: z.string(),
+    metadata: sentryUserMetadataSchema,
+});
+
+export type sentryUserSchemaType = z.infer<typeof sentryUserSchema>;
