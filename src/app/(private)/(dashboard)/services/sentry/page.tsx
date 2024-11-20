64b507c727e2beab7d1f23c363a795c588c15b7f
Lucas Charrier <lucharrier@gmail.com>
2024-11-20 19:23:56 +0100

    chore: add sentry account creation

---
+++ src/app/(private)/(dashboard)/services/sentry/page.tsx
@@ -0,0 +1,56 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+
+import AccountDetails from "@/components/Service/AccountDetails";
+import SentryServiceForm from "@/components/Service/SentryServiceForm";
+import { getServiceAccount } from "@/lib/kysely/queries/services";
+import { sentryServiceInfoToModel } from "@/models/mapper/sentryMapper";
+import { sentryUserSchemaType } from "@/models/sentry";
+import { SERVICES } from "@/models/services";
+import config from "@/server/config";
+import { authOptions } from "@/utils/authoptions";
+
+const buildLinkToSentryTeam = (
+    team: sentryUserSchemaType["metadata"]["teams"][0]
+) => {
+    return team.name ? (
+        <a href={`${config.SENTRY_WEBSITE_URL}/${team.slug}`} target="_blank">
+            {team.name}
+        </a>
+    ) : (
+        team.name
+    );
+};
+
+export default async function SentryPage() {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect("/login");
+    }
+
+    const rawAccount = await getServiceAccount(
+        session.user.uuid,
+        SERVICES.SENTRY
+    );
+    const service_account = rawAccount
+        ? sentryServiceInfoToModel(rawAccount)
+        : undefined;
+
+    return (
+        <>
+            <h1>Compte Sentry</h1>
+            {service_account ? (
+                <AccountDetails
+                    account={service_account}
+                    data={service_account.metadata.teams.map((team) => [
+                        buildLinkToSentryTeam(team),
+                        team.role,
+                    ])}
+                    headers={["nom", "niveau d'accès"]}
+                />
+            ) : (
+                <SentryServiceForm />
+            )}
+        </>
+    );
+}
--- src/app/api/services/actions.ts
+++ src/app/api/services/actions.ts
@@ -6,15 +6,20 @@ import { match } from "ts-pattern";
 
 import { db } from "@/lib/kysely";
 import { getUserBasicInfo, getUserStartups } from "@/lib/kysely/queries/users";
-import { matomoAccountRequestWrapperSchemaType } from "@/models/actions/service";
+import {
+    matomoAccountRequestWrapperSchemaType,
+    sentryAccountRequestWrapperSchemaType,
+} from "@/models/actions/service";
 import {
     CreateMattermostAccountDataSchema,
     CreateMatomoAccountDataSchema,
+    CreateSentryAccountDataSchema,
 } from "@/models/jobs/services";
 import { ACCOUNT_SERVICE_STATUS, SERVICES } from "@/models/services";
 import { encryptPassword } from "@/server/controllers/utils";
 import { getBossClientInstance } from "@/server/queueing/client";
 import { createMatomoServiceAccountTopic } from "@/server/queueing/workers/create-matomo-account";
+import { createSentryServiceAccountTopic } from "@/server/queueing/workers/create-sentry-account";
 import { authOptions } from "@/utils/authoptions";
 import {
     AuthorizationError,
@@ -29,6 +34,7 @@ export const askAccountCreationForService = withErrorHandling(
         data,
     }:
         | matomoAccountRequestWrapperSchemaType
+        | sentryAccountRequestWrapperSchemaType
         | { service: SERVICES.MATTERMOST; data: any }) => {
         // create task
         const session = await getServerSession(authOptions);
@@ -74,6 +80,41 @@ export const askAccountCreationForService = withErrorHandling(
                     })
                     .execute();
             })
+            .with(SERVICES.SENTRY, async () => {
+                if (!user.primary_email) {
+                    throw new ValidationError(
+                        "Un email primaire est obligatoire"
+                    );
+                }
+                await bossClient.send(
+                    createSentryServiceAccountTopic,
+                    CreateSentryAccountDataSchema.parse({
+                        email: user.primary_email,
+                        login: user.primary_email,
+                        password: encryptPassword(
+                            data.password ||
+                                crypto
+                                    .randomBytes(20)
+                                    .toString("base64")
+                                    .slice(0, -2)
+                        ),
+                    }),
+                    {
+                        retryLimit: 50,
+                        retryBackoff: true,
+                    }
+                );
+                await db
+                    .insertInto("service_accounts")
+                    .values({
+                        user_id: user.uuid,
+                        email: user.primary_email,
+                        account_type: SERVICES.SENTRY,
+                        status: ACCOUNT_SERVICE_STATUS.ACCOUNT_CREATION_PENDING,
+                    })
+                    .execute();
+            })
+
             .otherwise(() => {
                 // otherwise or exhaustive should be defined otherwise function is not awaited
                 // cf https://github.com/gvergnaud/ts-pattern/issues/163
