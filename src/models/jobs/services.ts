3052a6e9d58d6b05c9b856cb51b593346fd1fcc1
Lucas Charrier <lucharrier@gmail.com>
2024-11-20 19:23:56 +0100

    chore: add sentry account creation

--- src/models/jobs/services.ts
+++ src/models/jobs/services.ts
@@ -39,6 +39,7 @@ export type CreateMatomoAccountDataSchemaType = z.infer<
 
 export const CreateSentryAccountDataSchema =
     MaintenanceWrapperDataSchema.extend({
+<<<<<<< HEAD
         teams: z.array(
             z.object({
                 teamSlug: z.string(),
@@ -48,10 +49,18 @@ export const CreateSentryAccountDataSchema =
         email: z.string().email(),
         username: z.string(), // used to logged infortion
         userUuid: z.string(),
+=======
+        email: z.string().email(), // Valide que l'email est bien formaté
+        login: z.string().min(1, "Le nom d'utilisateur est requis"), // Valide que le nom d'utilisateur n'est pas vide
+        password: z
+            .string()
+            .min(6, "Le mot de passe doit contenir au moins 6 caractères"), // Valide que le mot de passe contient au moins 6 caractères
+>>>>>>> 64b507c7 (chore: add sentry account creation)
     }).strict();
 export type CreateSentryAccountDataSchemaType = z.infer<
     typeof CreateSentryAccountDataSchema
 >;
+<<<<<<< HEAD
 
 export const UpdateSentryAccountDataSchema =
     MaintenanceWrapperDataSchema.extend({
@@ -70,3 +79,5 @@ export const UpdateSentryAccountDataSchema =
 export type UpdateSentryAccountDataSchemaType = z.infer<
     typeof UpdateSentryAccountDataSchema
 >;
+=======
+>>>>>>> 64b507c7 (chore: add sentry account creation)
