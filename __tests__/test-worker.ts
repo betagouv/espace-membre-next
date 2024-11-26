bae070f942494a9106ef867070d8a3720954b040
Lucas Charrier <lucharrier@gmail.com>
2024-11-26 15:07:37 +0100

    test: add test sentry worker

--- __tests__/test-worker.ts
+++ __tests__/test-worker.ts
@@ -5,9 +5,13 @@ import sinon from "sinon";
 import testUsers from "./users.json";
 import utils from "./utils";
 import { db } from "@/lib/kysely";
-import { CreateMatomoAccountDataSchemaType } from "@/models/jobs/services";
+import {
+    CreateMatomoAccountDataSchemaType,
+    CreateSentryAccountDataSchemaType,
+} from "@/models/jobs/services";
 import { ACCOUNT_SERVICE_STATUS, SERVICES } from "@/models/services";
 import { createMatomoServiceAccount } from "@/server/queueing/workers/create-matomo-account";
+import { createSentryServiceAccount } from "@/server/queueing/workers/create-sentry-account";
 import * as controllerUtils from "@controllers/utils";
 
 describe("Service account creation by worker", () => {
@@ -46,7 +50,9 @@ describe("Service account creation by worker", () => {
                 .where("service_user_id", "=", "membre.actif@betagouv.ovh")
                 .where("account_type", "=", "matomo")
                 .executeTakeFirstOrThrow();
-            result.status?.should.equal(ACCOUNT_SERVICE_STATUS.ACCOUNT_FOUND);
+            result.status?.should.equal(
+                ACCOUNT_SERVICE_STATUS.ACCOUNT_INVITATION_SENT
+            );
         });
     });
 
@@ -94,6 +100,99 @@ describe("Service account creation by worker", () => {
                 .selectAll()
                 .where("user_id", "=", user?.uuid!)
                 .where("account_type", "=", "matomo")
+                .executeTakeFirstOrThrow();
+            account.should.exist;
+        });
+    });
+
+    describe("sentry account service consumer", () => {
+        let service_accounts;
+        before(async function () {
+            service_accounts = await db
+                .insertInto("service_accounts")
+                .values({
+                    email: "membre.actif@betagouv.ovh",
+                    account_type: "sentry",
+                    status: ACCOUNT_SERVICE_STATUS.ACCOUNT_CREATION_PENDING,
+                })
+                .executeTakeFirstOrThrow();
+        });
+        after(async function () {
+            await db
+                .deleteFrom("service_accounts")
+                .where("uuid", "=", service_accounts.uuid)
+                .executeTakeFirstOrThrow();
+        });
+        it("should create sentry service account", async () => {
+            await createSentryServiceAccount({
+                data: {
+                    email: "membre.actif@betagouv.ovh",
+                    userLogin: "membre.actif@betagouv.ovh",
+                    teams: [
+                        {
+                            teamSlug: "https://beta.gouv.fr",
+                            role: "admin",
+                        },
+                    ],
+                },
+            } as unknown as PgBoss.Job<CreateSentryAccountDataSchemaType>);
+
+            const result = await db
+                .selectFrom("service_accounts")
+                .selectAll()
+                .where("service_user_id", "=", "membre.actif@betagouv.ovh")
+                .where("account_type", "=", "sentry")
+                .executeTakeFirstOrThrow();
+            result.status?.should.equal(
+                ACCOUNT_SERVICE_STATUS.ACCOUNT_INVITATION_SENT
+            );
+        });
+    });
+
+    describe("sentry account producer", () => {
+        before(async () => {
+            await utils.createUsers(testUsers);
+        });
+        after(async () => {
+            await utils.deleteUsers(testUsers);
+        });
+
+        it("should create sentry worker tasks", async () => {
+            const user = await db
+                .selectFrom("users")
+                .where("username", "=", "membre.actif")
+                .selectAll()
+                .executeTakeFirst();
+            const mockSession = {
+                user: {
+                    id: "membre.actif",
+                    isAdmin: false,
+                    uuid: user?.uuid,
+                },
+            };
+            let getServerSessionStub = sinon.stub();
+            const askAccountCreationForService = proxyquire(
+                "@/app/api/services/actions",
+                {
+                    "next-auth": { getServerSession: getServerSessionStub },
+                }
+            ).askAccountCreationForService;
+            getServerSessionStub.resolves(mockSession);
+            await askAccountCreationForService({
+                service: SERVICES.SENTRY,
+                data: {
+                    teams: [
+                        {
+                            name: "beta.gouv.fr",
+                        },
+                    ],
+                },
+            });
+            const account = await db
+                .selectFrom("service_accounts")
+                .selectAll()
+                .where("user_id", "=", user?.uuid!)
+                .where("account_type", "=", "sentry")
                 .executeTakeFirstOrThrow();
             account.should.exist;
         });
