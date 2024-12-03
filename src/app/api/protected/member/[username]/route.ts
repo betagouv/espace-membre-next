2e5e991c409b662ac064cea80d0ce7ff89d56259
Lucas Charrier <lucharrier@gmail.com>
2024-12-03 11:09:14 +0100

    chore: protected api should send basic user info

--- src/app/api/protected/member/[username]/route.ts
+++ src/app/api/protected/member/[username]/route.ts
@@ -1,14 +1,19 @@
-import { HttpStatusCode } from 'axios';
+import { HttpStatusCode } from "axios";
 
-import { safeGetUserPublicInfo } from '@/app/api/member/actions';
+import { getUserBasicInfo } from "@/lib/kysely/queries/users";
+import { memberBaseInfoToModel } from "@/models/mapper";
 
 export async function GET(
     _: Request,
     { params: { username } }: { params: { username: string } }
 ) {
-    const userInfo = await safeGetUserPublicInfo(username);
-    if (userInfo.success) {
-        return Response.json(userInfo.data);
+    const dbUser = await getUserBasicInfo({ username });
+    if (!dbUser) {
+        return Response.json(
+            { error: "No user found for this username" },
+            { status: HttpStatusCode.NotFound }
+        );
     }
-    return Response.json({ error: userInfo.message }, { status: HttpStatusCode.NotFound });
+    const member = memberBaseInfoToModel(dbUser);
+    return Response.json(member);
 }
