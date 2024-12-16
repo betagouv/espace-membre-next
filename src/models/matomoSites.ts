ab8ddabc02865ac919c56213acd5fb1de78a3bf3
Lucas Charrier <lucharrier@gmail.com>
2024-12-16 15:50:05 +0100

    chore: add matomoSites and sentryTeam schema file

---
+++ src/models/matomoSites.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const matomoSiteSchema = z
+    .object({
+        id: z.string(),
+        matomo_id: z.number(),
+        name: z.string(),
+        startup_id: z.string().nullable(), // Nullable string
+        type: z.string(),
+        url: z.string().url(), // Valid URL
+    })
+    .strip();
+
+// Example usage to infer the type
+export type matomoSiteSchemaType = z.infer<typeof matomoSiteSchema>;
