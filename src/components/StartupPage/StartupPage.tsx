f69f70da1fbae4b890d48bbbcd1b3d04fd5dff0c
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-12-24 17:19:35 +0100

    wip

--- src/components/StartupPage/StartupPage.tsx
+++ src/components/StartupPage/StartupPage.tsx
@@ -1,43 +1,22 @@
 "use client";
 import Accordion from "@codegouvfr/react-dsfr/Accordion";
-import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { Table } from "@codegouvfr/react-dsfr/Table";
+import { Tabs } from "@codegouvfr/react-dsfr/Tabs";
+import { fr } from "@codegouvfr/react-dsfr/fr";
+import MarkdownIt from "markdown-it";
 
-import LastChange from "../LastChange";
 import { matomoSiteSchemaType } from "@/models/matomoSite";
 import { memberBaseInfoSchemaType } from "@/models/member";
 import { sentryTeamSchemaType } from "@/models/sentryTeam";
 import { phaseSchemaType, startupSchemaType } from "@/models/startup";
 import { StartupChangeSchemaType } from "@/models/startupChange";
-import { getLastMissionDate } from "@/utils/member";
 import { getCurrentPhase } from "@/utils/startup";
+import { StartupHeader } from "./StartupHeader";
+import { MemberTable } from "./MemberTable";
+import { getStartupFiles } from "@/app/api/startups/files/list";
+import { StartupFiles } from "../StartupFiles";
 
-function MemberTable({
-    members,
-    startup_id,
-}: {
-    members: memberBaseInfoSchemaType[];
-    startup_id: string;
-}) {
-    return (
-        <Table
-            data={members.map(
-                (member: memberBaseInfoSchemaType, index: number) => [
-                    <a key={index} href={`/community/${member.username}`}>
-                        {member.fullname}
-                    </a>,
-                    member.role,
-                    getLastMissionDate(
-                        member.missions.filter((mission) =>
-                            (mission.startups || []).includes(startup_id)
-                        )
-                    ) || "",
-                ]
-            )}
-            headers={["Nom", "Role", "Date de fin"]}
-        />
-    );
-}
+const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 export interface StartupPageProps {
     startupInfos: startupSchemaType;
@@ -46,6 +25,18 @@ export interface StartupPageProps {
     changes: StartupChangeSchemaType[];
     sentryTeams: sentryTeamSchemaType[];
     matomoSites: matomoSiteSchemaType[];
+    incubator: {
+        title: string;
+        uuid: string;
+        ghid: string | null;
+        short_description: string | null;
+    };
+    sponsors: {
+        uuid: string;
+        name: string;
+        acronym: string | null;
+    }[];
+    files: Awaited<ReturnType<typeof getStartupFiles>>;
 }
 
 export default function StartupPage({
@@ -55,6 +46,9 @@ export default function StartupPage({
     changes,
     matomoSites,
     sentryTeams,
+    incubator,
+    sponsors,
+    files,
 }: StartupPageProps) {
     const currentPhase = getCurrentPhase(phases); // todo get current phase
     const activeMembers = members.filter((member) =>
@@ -72,51 +66,157 @@ export default function StartupPage({
                 m.end < new Date()
         )
     );
+
+    const tabs = [
+        {
+            label: "Équipe",
+            isDefault: true,
+            content: (
+                <>
+                    <div className={fr.cx("fr-mb-2w")}>
+                        <a href={`mailto:${startupInfos.contact}`}>
+                            <i
+                                className={fr.cx(
+                                    "fr-icon--sm",
+                                    "fr-icon-mail-fill"
+                                )}
+                            />{" "}
+                            Contacter l'équipe
+                        </a>
+                    </div>
+                    <MemberTable
+                        members={activeMembers}
+                        startup_id={startupInfos.uuid}
+                    />
+                    <Accordion label="Anciens membres">
+                        <MemberTable
+                            members={previousMembers}
+                            startup_id={startupInfos.uuid}
+                        />
+                    </Accordion>
+                </>
+            ),
+        },
+        {
+            label: "Description",
+            content: (
+                <>
+                    <Table
+                        headers={["Nom", "Valeur"]}
+                        data={[
+                            ["Phase", currentPhase],
+                            [
+                                "Fiche beta.gouv.fr",
+                                <a
+                                    href={`https://beta.gouv.fr/startups/${startupInfos.ghid}`}
+                                    target="_blank"
+                                >
+                                    https://beta.gouv.fr/startups/
+                                    {startupInfos.ghid}
+                                </a>,
+                            ],
+                            startupInfos.repository && [
+                                "Code source",
+                                <a
+                                    href={startupInfos.repository}
+                                    target="_blank"
+                                >
+                                    {startupInfos.repository}
+                                </a>,
+                            ],
+                            [
+                                "Contact",
+                                <a href={`mailtor:${startupInfos.contact}`}>
+                                    {startupInfos.contact}
+                                </a>,
+                            ],
+                        ].filter((x) => !!x)}
+                    />
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: mdParser.render(startupInfos.description),
+                        }}
+                    />
+                </>
+            ),
+        },
+        {
+            label: "Historique",
+            content: "[TODO]",
+        },
+        {
+            label: "Standards",
+            content: "[TODO]",
+        },
+        {
+            label: "Outils",
+            content: (
+                <div className="fr-mb-4v">
+                    <h3>Outils</h3>
+                    <Accordion
+                        label="Matomo"
+                        expanded={true}
+                        onExpandedChange={(expanded, e) => {}}
+                    >
+                        {!matomoSites.length && (
+                            <p>Aucun site matomo n'est connecté à ce produit</p>
+                        )}
+                        {!!matomoSites.length && (
+                            <Table
+                                data={matomoSites.map((site) => [
+                                    site.name,
+                                    site.url,
+                                    site.type,
+                                ])}
+                                headers={["nom du site", "url", "type"]}
+                            ></Table>
+                        )}
+                    </Accordion>
+                    <Accordion
+                        label="Sentry"
+                        expanded={true}
+                        onExpandedChange={(expanded, e) => {}}
+                    >
+                        {!sentryTeams.length && (
+                            <p>
+                                Aucun équipe sentry n'est connecté à ce produit
+                            </p>
+                        )}
+                        {!!sentryTeams.length && (
+                            <Table
+                                data={sentryTeams.map((site) => [site.name])}
+                                headers={["nom de l'équipe"]}
+                            ></Table>
+                        )}
+                    </Accordion>
+                </div>
+            ),
+        },
+        {
+            label: "Documents",
+            content: (
+                <>
+                    <StartupFiles startup={startupInfos} files={files} />
+                </>
+            ),
+        },
+    ];
+
     return (
         <>
-            <div className="fr-mb-8v">
-                <h1>{startupInfos.name}</h1>
-                <LastChange changes={changes} />
-                <p>
-                    <span>
-                        Fiche GitHub :{" "}
-                        <a
-                            className="fr-link"
-                            target="_blank"
-                            href={`https://github.com/betagouv/beta.gouv.fr/blob/master/content/_startups/${startupInfos.ghid}.md`}
-                        >
-                            {startupInfos.name}
-                        </a>
-                    </span>
-                    <br />
-                    <span>
-                        Repository :{" "}
-                        {startupInfos.repository ? (
-                            <a
-                                className="fr-link"
-                                target="_blank"
-                                href={startupInfos.repository}
-                            >
-                                {startupInfos.repository}
-                            </a>
-                        ) : (
-                            "Non renseigné"
-                        )}
-                    </span>
-                    <br />
-                    <span>
-                        Contact :{" "}
-                        {startupInfos.contact && (
-                            <a href={`mailto:${startupInfos.contact}`}>
-                                {startupInfos.contact}
-                            </a>
-                        )}
-                    </span>
-                    <br />
-                    <span>Phase : {currentPhase}</span>
-                    <br />
-                </p>
-                <p className="fr-text--sm" style={{ fontStyle: "italic" }}>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+                <StartupHeader
+                    startupInfos={startupInfos}
+                    changes={changes}
+                    incubator={incubator}
+                    sponsors={sponsors}
+                />
+
+                <div className={fr.cx("fr-col-12")}>
+                    <Tabs tabs={tabs}></Tabs>
+                </div>
+
+                {/* <p className="fr-text--sm" style={{ fontStyle: "italic" }}>
                     Une information n'est pas à jour ?
                 </p>
                 <ButtonsGroup
@@ -138,60 +238,7 @@ export default function StartupPage({
                             priority: "secondary",
                         },
                     ]}
-                ></ButtonsGroup>
-            </div>
-
-            <div className="fr-mb-4v">
-                <h3>Membres</h3>
-                <Accordion
-                    label="Membres actuels"
-                    expanded={true}
-                    onExpandedChange={(expanded, e) => {}}
-                >
-                    <MemberTable
-                        members={activeMembers}
-                        startup_id={startupInfos.uuid}
-                    />
-                </Accordion>
-                <Accordion label="Membres précédents">
-                    <MemberTable
-                        members={previousMembers}
-                        startup_id={startupInfos.uuid}
-                    />
-                </Accordion>
-            </div>
-            <div className="fr-mb-4v">
-                <h3>Outils</h3>
-                <Accordion
-                    label="Matomo"
-                    expanded={true}
-                    onExpandedChange={(expanded, e) => {}}
-                >
-                    {!matomoSites.length && (
-                        <p>Aucun site matomo n'est connecté à ce produit</p>
-                    )}
-                    {!!matomoSites.length && (
-                        <Table
-                            data={matomoSites.map((site) => [
-                                site.name,
-                                site.url,
-                                site.type,
-                            ])}
-                            headers={["nom du site", "url", "type"]}
-                        ></Table>
-                    )}
-                </Accordion>
-                <Accordion label="Sentry">
-                    {!sentryTeams.length && (
-                        <p>Aucun équipe sentry n'est connecté à ce produit</p>
-                    )}
-                    {!!sentryTeams.length && (
-                        <Table
-                            data={sentryTeams.map((site) => [site.name])}
-                            headers={["nom de l'équipe"]}
-                        ></Table>
-                    )}
-                </Accordion>
+                ></ButtonsGroup> */}
             </div>
         </>
     );
