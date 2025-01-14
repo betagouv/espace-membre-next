e90bec397b3c82c2bb04b392b4a73ecba87d7619
Lucas Charrier <lucharrier@gmail.com>
2025-01-14 17:49:31 +0100

    chore: removed legacy manageSecondaryEmailAction

--- src/server/controllers/usersController/index.ts
+++ src/server/controllers/usersController/index.ts
@@ -1,6 +1,5 @@
 export * from "./createRedirectionForUser";
 export * from "./deleteRedirectionForUser";
-export * from "./manageSecondaryEmailForUser";
 export * from "./updatePasswordForUser";
 export * from "./usersControllerUtils";
 export * from "./upgradeEmailForUser";
