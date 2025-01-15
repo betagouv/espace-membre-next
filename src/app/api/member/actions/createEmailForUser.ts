b77342d690ae3cced85b22e231aaa0eb3ddcd843
Lucas Charrier <lucharrier@gmail.com>
2025-01-15 14:33:52 +0100

    chore: add files

---
+++ src/app/api/member/actions/createEmailForUser.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { getServerSession } from "next-auth/next";
+
+import { createEmailForUser } from "@/server/controllers/usersController/createEmailForUser";
+import { authOptions } from "@/utils/authoptions";
+import {
+    AuthorizationError,
+    UnwrapPromise,
+    withErrorHandling,
+} from "@/utils/error";
+
+export async function createEmail({
+    username,
+    to_email,
+}: {
+    username: string;
+    to_email: string;
+}) {
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user.id) {
+        throw new AuthorizationError();
+    }
+
+    await createEmailForUser({ username }, session.user.id);
+}
