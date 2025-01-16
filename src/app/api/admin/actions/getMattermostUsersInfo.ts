507b64e958406cf16e13e9d16b856ed35ebb884e
Lucas Charrier <lucharrier@gmail.com>
2025-01-16 17:58:08 +0100

    chore: removed imports and use server

--- src/app/api/admin/actions/getMattermostUsersInfo.ts
+++ src/app/api/admin/actions/getMattermostUsersInfo.ts
@@ -2,11 +2,6 @@
 
 import { getServerSession } from "next-auth/next";
 
-import { addEvent } from "@/lib/events";
-import { MattermostChannel } from "@/lib/mattermost";
-import { EventCode } from "@/models/actionEvent/actionEvent";
-("use server");
-
 import { MattermostUser } from "@/models/mattermost";
 import { getMattermostUsers } from "@/server/controllers/adminController";
 import { authOptions } from "@/utils/authoptions";
