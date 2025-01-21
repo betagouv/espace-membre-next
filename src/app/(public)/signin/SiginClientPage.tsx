07345ae7676ae53a877262f111d36810ba393682
Charrier <lucharrier@gmail.com>
2025-01-21 18:08:25 +0100

    Revert "chore: remove autoSubmit for francetravail (#742)" (#745)
    This reverts commit 25231f4e65afa0963a41290bd48f32f38e13f96f.

--- src/app/(public)/signin/SiginClientPage.tsx
+++ src/app/(public)/signin/SiginClientPage.tsx
@@ -67,6 +67,10 @@ export default function SignClientPage() {
         document.location = "/";
     };
 
+    React.useEffect(() => {
+        if (typeof window !== "undefined") onSubmit();
+    }, [onSubmit]);
+
     return (
         <div>
             <p className={fr.cx("fr-mt-10v")}>
