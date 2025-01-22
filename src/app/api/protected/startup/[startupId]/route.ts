35a1d6a9271f91fcac3f1e58781432c198bb9da5
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-01-22 11:00:13 +0100

    feat(api): add protected get one startup

---
+++ src/app/api/protected/startup/[startupId]/route.ts
@@ -0,0 +1,28 @@
+import { HttpStatusCode } from 'axios';
+import { NextRequest } from 'next/server';
+
+import { getStartup } from '@/lib/kysely/queries';
+import { getIncubator } from '@/lib/kysely/queries/incubators';
+import { incubatorToModel, startupToModel } from '@/models/mapper';
+
+export const GET = async (
+    _: NextRequest,
+    { params: { startupId } }: { params: { startupId: string } }
+) => {
+    const dbStartup = await getStartup({ghid: startupId});
+
+    if (!dbStartup) {
+        return Response.json(
+            { error: "No startup found for this id" },
+            { status: HttpStatusCode.NotFound }
+        );
+    }
+
+    const startup = startupToModel(dbStartup);
+    const incubator = incubatorToModel(await getIncubator(startup.incubator_id));
+
+    return Response.json({
+        ...startup,
+        incubator,
+    });
+}
