233e15f7efaca30f26a8ed4614b8dd6a56eda3c2
Lucas Charrier <lucharrier@gmail.com>
2025-01-28 17:27:57 +0100

    tests: added create sentry team test

--- __tests__/test-worker.ts
+++ __tests__/test-worker.ts
@@ -5,13 +5,16 @@ import sinon from "sinon";
 import testUsers from "./users.json";
 import utils from "./utils";
 import { db } from "@/lib/kysely";
+import { EventCode } from "@/models/actionEvent";
 import { MATOMO_SITE_TYPE } from "@/models/actions/service";
 import {
     CreateOrUpdateMatomoAccountDataSchemaType,
     CreateSentryAccountDataSchemaType,
+    CreateSentryTeamDataSchemaType,
 } from "@/models/jobs/services";
 import { ACCOUNT_SERVICE_STATUS, SERVICES } from "@/models/services";
 import { createSentryServiceAccount } from "@/server/queueing/workers/create-sentry-account";
+import { createSentryTeam } from "@/server/queueing/workers/create-sentry-team";
 import { createOrUpdateMatomoServiceAccount } from "@/server/queueing/workers/create-update-matomo-account";
 import * as controllerUtils from "@controllers/utils";
 
@@ -113,6 +116,7 @@ describe("Service account creation by worker", () => {
     describe("sentry account service consumer", () => {
         let service_accounts;
         let user;
+        let newStartup;
         before(async function () {
             await utils.createUsers(testUsers);
             user = await db
@@ -130,6 +134,14 @@ describe("Service account creation by worker", () => {
                     status: ACCOUNT_SERVICE_STATUS.ACCOUNT_CREATION_PENDING,
                 })
                 .executeTakeFirstOrThrow();
+            newStartup = await db
+                .insertInto("startups")
+                .values({
+                    name: "a-startup",
+                    ghid: "a-startup",
+                })
+                .returning("uuid")
+                .executeTakeFirstOrThrow();
         });
         after(async function () {
             await db
@@ -137,6 +149,10 @@ describe("Service account creation by worker", () => {
                 .where("uuid", "=", service_accounts.uuid)
                 .executeTakeFirstOrThrow();
             await utils.deleteUsers(testUsers);
+            await db
+                .deleteFrom("startups")
+                .where("uuid", "=", newStartup.uuid)
+                .execute();
         });
         it("should create sentry service account", async () => {
             await createSentryServiceAccount({
@@ -164,14 +180,53 @@ describe("Service account creation by worker", () => {
                 ACCOUNT_SERVICE_STATUS.ACCOUNT_INVITATION_SENT
             );
         });
+        it("should create sentry team", async () => {
+            await createSentryTeam({
+                data: {
+                    email: "membre.actif@betagouv.ovh",
+                    userLogin: "membre.actif@betagouv.ovh",
+                    username: "membre.actif",
+                    userUuid: user?.uuid,
+                    startupId: newStartup.uuid,
+                    teams: [
+                        {
+                            teamSlug: "a-startup",
+                            role: "admin",
+                        },
+                    ],
+                },
+            } as unknown as PgBoss.Job<CreateSentryTeamDataSchemaType>);
+
+            const result = await db
+                .selectFrom("events")
+                .selectAll()
+                .orderBy("created_at desc")
+                .executeTakeFirst();
+            result?.action_code.should.equal(
+                EventCode.MEMBER_SERVICE_TEAM_CREATED
+            );
+        });
     });
 
     describe("sentry account producer", () => {
+        let newStartup;
         before(async () => {
             await utils.createUsers(testUsers);
+            newStartup = await db
+                .insertInto("startups")
+                .values({
+                    name: "a-startup",
+                    ghid: "a-startup",
+                })
+                .returning("uuid")
+                .executeTakeFirstOrThrow();
         });
         after(async () => {
             await utils.deleteUsers(testUsers);
+            await db
+                .deleteFrom("startups")
+                .where("uuid", "=", newStartup.uuid)
+                .execute();
         });
 
         it("should create sentry worker tasks", async () => {
@@ -211,6 +266,55 @@ describe("Service account creation by worker", () => {
                 .where("user_id", "=", user?.uuid!)
                 .where("account_type", "=", "sentry")
                 .executeTakeFirstOrThrow();
+            account.should.exist;
+        });
+
+        it("should create sentry worker tasks create account and create team", async () => {
+            const user = await db
+                .selectFrom("users")
+                .where("username", "=", "membre.actif")
+                .selectAll()
+                .executeTakeFirst();
+            const mockSession = {
+                user: {
+                    id: "membre.actif",
+                    isAdmin: false,
+                    uuid: user?.uuid,
+                },
+            };
+            let getServerSessionStub = sinon.stub();
+            const askAccountCreationForService = proxyquire(
+                "@/app/api/services/actions",
+                {
+                    "next-auth": { getServerSession: getServerSessionStub },
+                }
+            ).askAccountCreationForService;
+            getServerSessionStub.resolves(mockSession);
+            await askAccountCreationForService({
+                service: SERVICES.SENTRY,
+                data: {
+                    newTeam: {
+                        startupId: newStartup.uuid,
+                    },
+                },
+            });
+            const account = await db
+                .selectFrom("service_accounts")
+                .selectAll()
+                .where("user_id", "=", user?.uuid!)
+                .where("account_type", "=", "sentry")
+                .executeTakeFirstOrThrow();
+            const events = await db
+                .selectFrom("events")
+                .selectAll()
+                .orderBy("created_at desc")
+                .execute();
+            events[0].action_code.should.equal(
+                EventCode.MEMBER_SERVICE_ACCOUNT_REQUESTED
+            );
+            events[1].action_code.should.equal(
+                EventCode.MEMBER_SERVICE_TEAM_CREATION_REQUESTED
+            );
             account.should.exist;
         });
     });
