d9a0d19785098d023529b434b794f52443f27a3e
Charrier <lucharrier@gmail.com>
2025-01-29 14:20:42 +0100

    chore: replace mattermost route by server action (#769)
    * chore: replace mattermost route by server action
    
    * removed mattermost admin
    
    * chore: add mattermost server action files
    
    * chore: delete no more usefull mattermost route files
    
    * chore: removed admin router from index file
    
    * chore: use optionnal chaining
    
    * chore: add getMattermostAdmin and fix imports
    
    * chore: set isLoading to fale to display mattermost interface
    
    * Update src/components/AdminMattermostPage/AdminMattermost.tsx
    
    Co-authored-by: Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
    
    * Update src/components/AdminMattermostPage/AdminMattermost.tsx
    
    Co-authored-by: Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
    
    ---------
    
    Co-authored-by: Julien Bouquillon <julien.bouquillon@beta.gouv.fr>

--- src/components/MemberPage/MemberEmailServiceInfo.tsx
+++ src/components/MemberPage/MemberEmailServiceInfo.tsx
@@ -5,7 +5,7 @@ import Button from "@codegouvfr/react-dsfr/Button";
 import {
     unblockMemberEmailAddress,
     unblockMemberEmailAddressFromCampaign,
-} from "@/app/api/admin/action";
+} from "@/app/api/admin/actions";
 import {
     brevoEmailInfoDataSchema,
     brevoEmailInfoDataSchemaType,
