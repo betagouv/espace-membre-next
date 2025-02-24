40d90ac8bc0d1341d7ea4736e86261a1e333c50f
Lucas Charrier <lucharrier@gmail.com>
2025-02-24 23:36:29 +0100

    feat(n): use https

--- start-and-wait-to-init.sh
+++ start-and-wait-to-init.sh
@@ -25,7 +25,7 @@ check_server_and_init() {
   counter=0
 
   host="$FULL_HOSTNAME"
-  healthcheck_url="http://$host"
+  healthcheck_url="https://$host"
 
   while true; do
     response=$(curl --write-out %{http_code} --silent --output /dev/null "$healthcheck_url")
@@ -47,7 +47,7 @@ check_server_and_init() {
     counter=$((counter+1))
   done
     echo "Will curl init"
-  curl http://$host/api/init
+  curl https://$host/api/init
 }
 
 # In parallel wait for the server readiness to init some services
