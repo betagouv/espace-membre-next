e272b424d104a728f20b7d1c4211e39da5546b12
Lucas Charrier <lucharrier@gmail.com>
2025-02-25 18:03:46 +0100

    chore: add back dashboard page as it was

--- src/app/(private)/(dashboard)/dashboard/page.tsx
+++ src/app/(private)/(dashboard)/dashboard/page.tsx
@@ -3,13 +3,16 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
-import { DashboardPage } from "./DashboardPage";
 import { SURVEY_BOX_COOKIE_NAME } from "@/components/SurveyBox";
 import { getUserInfos } from "@/lib/kysely/queries/users";
+import { getLatests as getLatestsMembers } from "@/lib/kysely/queries/users";
 import { userInfosToModel } from "@/models/mapper";
 import { EmailStatusCode } from "@/models/member";
 import { authOptions } from "@/utils/authoptions";
 import { routeTitles } from "@/utils/routes/routeTitles";
+
+import { DashboardPage } from "@/components/Dashboard/DashboardPage";
+import { getLatests as getLatestsStartups } from "@/lib/kysely/queries/startups";
 
 export const metadata: Metadata = {
     title: `${routeTitles.dashboard()} / Espace Membre`,
@@ -34,9 +37,18 @@ export default async function Page(props) {
         return redirect("/verify");
     }
 
+    const lastestProducts = await getLatestsStartups();
+    const lastestMembers = await getLatestsMembers();
     const cookieStore = cookies();
     const surveyCookie = cookieStore.get(SURVEY_BOX_COOKIE_NAME);
     const surveyCookieValue = (surveyCookie && surveyCookie.value) || null;
 
-    return <DashboardPage {...props} surveyCookieValue={surveyCookieValue} />;
+    return (
+        <DashboardPage
+            {...props}
+            surveyCookieValue={surveyCookieValue}
+            latestProducts={lastestProducts}
+            latestMembers={lastestMembers}
+        />
+    );
 }
