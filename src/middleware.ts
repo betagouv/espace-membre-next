ccb01517126ea278845f2b2bd09ca688781aad79
Charrier <lucharrier@gmail.com>
2025-02-26 15:52:50 +0100

    Apply suggestions from code review

--- src/middleware.ts
+++ src/middleware.ts
@@ -116,7 +116,9 @@ export async function middleware(req: NextRequest) {
         // otherwise, redirect to the set token page
         else {
             return NextResponse.redirect(
-                new URL(`/login?next=${req.url}`, req.url)
+            // /!\ redirecting to pathname as req.url always use localhost:3000 as hostname
+            return NextResponse.redirect(
+                new URL(`/login?next=${req.nextUrl.pathname}`, req.url)
             );
         }
     }
