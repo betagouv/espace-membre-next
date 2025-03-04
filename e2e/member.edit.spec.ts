f4eba5771c7da1f96eeeeddf76cd55f2d8864639
Charrier <lucharrier@gmail.com>
2025-03-04 16:26:13 +0100

    feat: prevent update of more than 6 months in the future for non "Agent Public" users (#811)
    * feat: prevent update of more than 6 months in the future for non admin
    
    * fix: add missing second parameter
    
    * chore: link mission message issue to the specific mission error in zod
    
    * chore: add playwright tests
    
    * test: modify playwright test change YYYY to yyyy in date formating
    
    * test(playwright): give right format to date input
    
    * feat: add missions check on updateMemberMissionsSchema

--- e2e/member.edit.spec.ts
+++ e2e/member.edit.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from "@playwright/test";
+import { addMonths } from "date-fns/addMonths";
+import { format } from "date-fns/format";
 
 test.use({ storageState: "./playwright-auth-valid.member.json" });
 
@@ -23,4 +25,34 @@ test("can edit my account", async ({ page }) => {
     await page.waitForURL("/account");
 
     expect(await page.getByText(randomBio));
+});
+
+test("cannot add date with more than 6 months if status is independant", async ({
+    page,
+}) => {
+    const randomBio = "io " + Math.random();
+    const dateIn8Months = addMonths(new Date(), 8);
+
+    await page.goto("/account");
+
+    await page.getByText("Modifier la fiche").first().click();
+
+    await page.waitForURL("/account/base-info");
+
+    await page.getByLabel("Courte bio").fill(randomBio);
+    await page
+        .getByLabel("Type de contrat (obligatoire)")
+        .selectOption("Indépendant");
+    await page
+        .getByLabel("Date de fin de mission (obligatoire)")
+        .fill(format(dateIn8Months, "yyyy-MM-dd"));
+    await page.waitForTimeout(100);
+
+    await page.getByText("Enregistrer").click();
+
+    expect(
+        await page.getByText(
+            "La date de fin de mission ne peut pas être supérieure à 6 mois dans le futur."
+        )
+    );
 });
