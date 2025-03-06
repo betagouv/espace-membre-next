4b0a04b69b5d064bcd6f24aa495161cdc079dcf1
Lucas Charrier <lucharrier@gmail.com>
2025-03-06 15:56:37 +0100

    chore: add app mode

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,11 +1,14 @@
 // An empty DSN will disable Sentry
+
+import frontConfig from "@/frontConfig";
+
 // We want it to be enabled only when deployed
 export const dsn =
     process.env.NODE_ENV === "production"
         ? process.env.NEXT_PUBLIC_SENTRY_DSN
         : undefined;
 
-export const environment = process.env.NEXT_PUBLIC_APP_MODE;
+export const environment = process.env.NEXT_PUBLIC_APP_MODE || "prod";
 
 // During runtime this must match the value from the build so there is a connection to uploaded source maps
 // The following will be overriden by an hardcoded value as wanted thanks to Next.js `env` property
