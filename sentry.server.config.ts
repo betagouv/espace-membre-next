e68c71ce172082cd55a809f2d084a92e29f73a5a
Charrier <lucharrier@gmail.com>
2025-03-11 10:51:45 +0100

    Revert "config: init sentry according to nextjs advices (#830)" (#843)
    This reverts commit 8d9437b555b8ac99ef8e84bf2cea909a99376f94.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,15 +2,9 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    integrations: [Sentry.replayIntegration()],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 0.5,
-
-    // Capture Replay for 10% of all sessions,
-    // plus for 100% of sessions with an error
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1,
 });
