7090b10fa291e69adc1cb7ea506bf68531e4d7ff
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2025-04-02 14:15:17 +0200

    fix: fix incubator form error display fix #834

--- src/models/incubator.ts
+++ src/models/incubator.ts
@@ -3,15 +3,21 @@ import { z } from "zod";
 export const incubatorSchema = z.object({
     uuid: z.string(),
     // id: z.number(),
-    title: z.string().min(1),
+    title: z.string().min(3, "Le nom de l'incubateur est obligatoire"),
     owner_id: z.string().optional(),
-    contact: z.string().email().nullable().optional(),
-    ghid: z.string().min(2),
+    contact: z
+        .string()
+        .email({ message: "Email invalide" })
+        .nullable()
+        .optional(),
+    ghid: z.string().min(2, "Un acronyme est obligatoire"),
     address: z.string().nullable().optional(),
     website: z.string().url().optional().or(z.literal("")),
     github: z.string().url().optional().or(z.literal("")),
-    description: z.string(),
-    short_description: z.string(),
+    description: z.string().min(100, "Une description est obligatoire"),
+    short_description: z
+        .string()
+        .min(30, "Une description courte est obligatoire"),
     highlighted_startups: z.array(z.string().uuid()).optional(),
     organization_name: z.string().optional(),
 });
