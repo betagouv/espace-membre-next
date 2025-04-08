dffddb20e2bee4a885afb2600575d178fd79f4de
Lucas Charrier <lucharrier@gmail.com>
2025-04-08 11:32:43 +0200

    feat: add onboarding checklist model

---
+++ src/models/onboardingCheklist.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+import { Domaine } from "./member";
+
+export const itemSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    domaines: z.array(z.nativeEnum(Domaine)).optional(),
+});
+
+export const sectionSchema = z.object({
+    title: z.string(),
+    items: z.array(itemSchema),
+    domaines: z.array(z.nativeEnum(Domaine)).optional(),
+});
+
+export const onboardingChecklistSchema = z.array(sectionSchema);
+
+export type onboardingChecklistSchemaType = z.infer<
+    typeof onboardingChecklistSchema
+>;
