5e95c7d4b5db8503796d47fb305912b90d62377f
Lucas Charrier <lucharrier@gmail.com>
2025-04-08 09:02:39 +0200

    feat: add userEvent models

---
+++ src/models/userEvent.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const userEventSchema = z.object({
+    uuid: z.string().uuid(), // généré automatiquement
+    field_id: z.string().min(1), // non nullable
+    date: z.date().nullable(), // nullable
+    user_id: z.string().uuid(), // non nullable
+    created_at: z.date(), // généré par knex
+    updated_at: z.date(), // généré par knex
+});
+
+export type userEventSchemaType = z.infer<typeof userEventSchema>;
