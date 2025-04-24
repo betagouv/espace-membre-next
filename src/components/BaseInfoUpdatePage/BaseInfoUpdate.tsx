69c2ed4e10f55f1d6d250ce42f9a0d51459acd8a
Lucas Charrier <lucharrier@gmail.com>
2025-04-24 12:30:51 +0200

    chore: add margin bottom

--- src/components/BaseInfoUpdatePage/BaseInfoUpdate.tsx
+++ src/components/BaseInfoUpdatePage/BaseInfoUpdate.tsx
@@ -335,7 +335,7 @@ export const BaseInfoUpdate = (props: BaseInfoUpdateProps) => {
                         {isCurrentUser ? `Mes compétences` : `Compétences`}
                     </h2>
                     {isCurrentUser && (
-                        <label className="fr-label" htmlFor="competence-editor">
+                        <label className="fr-label fr-mb-3w" htmlFor="competence-editor">
                             Aide les membres de la communauté à mieux
                             t'identifier en cas de besoin
                         </label>
