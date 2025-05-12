8a5ccdb5c3b97e30deab5d01386825b24e783010
Lucas Charrier <lucharrier@gmail.com>
2025-05-12 18:26:32 +0200

    chore: add readable format for new email status

--- src/models/misc.ts
+++ src/models/misc.ts
@@ -12,8 +12,7 @@ export interface Option {
 
 export const EMAIL_STATUS_READABLE_FORMAT: Record<EmailStatusCode, string> = {
     EMAIL_ACTIVE: "Actif",
-    EMAIL_ACTIVE_AND_PASSWORD_DEFINITION_PENDING:
-        "Actif : en attente de définition du mot de passe",
+    EMAIL_ACTIVE_AND_PASSWORD_DEFINITION_PENDING: "Actif : en attente de définition du mot de passe",
     EMAIL_REDIRECTION_ACTIVE: `L'email est une redirection. Email attributaire`,
     EMAIL_SUSPENDED: "Suspendu",
     EMAIL_DELETED: "Supprimé",
@@ -21,14 +20,11 @@ export const EMAIL_STATUS_READABLE_FORMAT: Record<EmailStatusCode, string> = {
     EMAIL_CREATION_PENDING: "Création en cours",
     EMAIL_RECREATION_PENDING: "Recréation en cours",
     EMAIL_UNSET: "Non défini",
-    [EmailStatusCode.EMAIL_REDIRECTION_PENDING]:
-        "L'email est une redirection. La création est en cours",
-    [EmailStatusCode.EMAIL_VERIFICATION_WAITING]:
-        "Le membre doit se connecter à l'espace-membre et vérifier ses informations avant que tu puisses lui créer un compte.",
-    [EmailStatusCode.EMAIL_CREATION_WAITING]:
-        "La création de l'email est en cours",
-    [EmailStatusCode.MEMBER_VALIDATION_WAITING]:
-        "En attente de la validation par un responsable transverse",
+    [EmailStatusCode.EMAIL_REDIRECTION_PENDING]: "L'email est une redirection. La création est en cours",
+    [EmailStatusCode.EMAIL_VERIFICATION_WAITING]: "Le membre doit se connecter à l'espace-membre et vérifier ses informations avant que tu puisses lui créer un compte.",
+    [EmailStatusCode.EMAIL_CREATION_WAITING]: "La création de l'email est en cours",
+    [EmailStatusCode.MEMBER_VALIDATION_WAITING]: "En attente de la validation par un responsable transverse",
+    [EmailStatusCode.EMAIL_ACTIF_CREATION_WAITING_AT_OPI]: "Actif et en attente de création chez opi"
 };
 
 export interface FormErrorResponse {
