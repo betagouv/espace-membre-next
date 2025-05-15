7ae1d7fb1e179607653b82ac89905feee70984e9
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2025-05-15 17:02:16 +0200

    clean

--- src/app/(private)/(dashboard)/startups/page.tsx
+++ src/app/(private)/(dashboard)/startups/page.tsx
@@ -4,12 +4,16 @@ import { StartupType } from "@/components/SESelect";
 import { StartupList } from "@/components/StartupListPage";
 import { getAllStartups } from "@/lib/kysely/queries";
 import { routeTitles } from "@/utils/routes/routeTitles";
+import { getStartupFiles } from "@/app/api/startups/files/list";
+
+import { SearchFiles } from "./SearchFiles";
 
 export const metadata: Metadata = {
     title: `${routeTitles.startupList()} / Espace Membre`,
 };
 
 export default async function Page() {
+    const files = await getStartupFiles();
     const startups = await getAllStartups().then((r) =>
         r.map(
             (s) =>
@@ -23,6 +27,9 @@ export default async function Page() {
         <>
             <h1>{routeTitles.startupList()}</h1>
             <StartupList startups={startups} />
+            <hr />
+            <h2>{routeTitles.startupsFiles()}</h2>
+            <SearchFiles files={files} />
         </>
     );
 }
