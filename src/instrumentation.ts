bd60d668cef3cc3fcf62575ff35e51d7cb8040fc
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2025-09-02 18:51:50 +0200

    fix: upgrade sentry config

---
+++ src/instrumentation.ts
@@ -0,0 +1,20 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+      // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+      tracesSampleRate: 0.5,
+
+      // Enable logs to be sent to Sentry
+      enableLogs: true,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
+  }
+}
+
+export const onRequestError = Sentry.captureRequestError;
