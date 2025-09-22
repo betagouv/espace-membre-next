0d10ca39ebf89da219e03a2b3b6e479d82c1e6cd
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2025-09-22 20:03:10 +0200

    chore: rm keskispasse page (#1108)
    * chore: rm keskispasse page
    
    * keskispasse: add explanations+links
    
    * fix: tutoy

--- src/app/(public)/keskispasse/page.tsx
+++ src/app/(public)/keskispasse/page.tsx
@@ -1,22 +1,35 @@
 import type { Metadata } from "next";
 
-import { WhatIsGoingOnWithMember } from "@/components/WhatIsGoingOnWithMemberPage";
-// import { getAllUsersPublicInfo } from "@/server/db/dbUser";
-import { getAllStartups } from "@/lib/kysely/queries";
-import { getAllUsersInfo } from "@/lib/kysely/queries/users";
-import { memberPublicInfoToModel, startupToModel } from "@/models/mapper";
 import { routeTitles } from "@/utils/routes/routeTitles";
+import { fr } from "@codegouvfr/react-dsfr";
 
 export const metadata: Metadata = {
   title: `${routeTitles.keskispasse()} / Espace Membre`,
 };
 
 export default async function Page() {
-  const usersInfos = (await getAllUsersInfo()).map((user) =>
-    memberPublicInfoToModel(user),
+  return (
+    <div>
+      <br />
+      <h1>En cas de problème</h1>
+      <br />
+      <br />
+      <ul className={fr.cx("fr-text--xl")}>
+        <li>
+          Retrouve toutes les informations sur le statut d'un membre{" "}
+          <a href="/community">sur sa fiche</a>
+        </li>
+        <li>
+          Consulte{" "}
+          <a href="https://faq-betagouv.crisp.help/fr/category/espace-membre-1o8xzu0/">
+            la FAQ espace-membre
+          </a>
+        </li>
+        <li>
+          Contacte l'équipe support{" "}
+          <a href="https://faq-betagouv.crisp.help">via CRISP</a>
+        </li>
+      </ul>
+    </div>
   );
-  const startups = (await getAllStartups()).map((startup) =>
-    startupToModel(startup),
-  );
-  return <WhatIsGoingOnWithMember startups={startups} users={usersInfos} />;
 }
