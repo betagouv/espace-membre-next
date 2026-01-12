6804d9a504ba25953473c2eb5e49196c277a8426
Julien Bouquillon <julien@bouquillon.com>
2026-01-12 14:35:19 +0100

    chore: disable revalidate
    another try to fix the css cache issue https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config\#revalidate

--- src/app/(private)/(dashboard)/layout.tsx
+++ src/app/(private)/(dashboard)/layout.tsx
@@ -6,6 +6,8 @@ import { authOptions } from "@/utils/authoptions";
 import { redirect } from "next/navigation";
 import React from "react";
 
+export const revalidate = 0;
+
 export default async function Layout(props: PropsWithChildren) {
   const session = await getServerSession(authOptions);
   if (!session || !session?.user.id) {
