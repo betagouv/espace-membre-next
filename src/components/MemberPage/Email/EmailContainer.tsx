f2c1dfd5194c2625ba2f3d4e0b970dec7c4f4ff9
Julien Bouquillon <julien@bouquillon.com>
2026-02-10 23:10:03 +0100

    fix: dimail invite

--- src/components/MemberPage/Email/EmailContainer.tsx
+++ src/components/MemberPage/Email/EmailContainer.tsx
@@ -14,7 +14,6 @@ import BlocConfigurerEmailPrincipal from "./BlocConfigurerEmailPrincipal";
 import BlocConfigurerEmailSecondaire from "./BlocConfigurerEmailSecondaire";
 import { WebMailButtons } from "./WebMailButtons";
 import { MemberPageProps } from "../MemberPage";
-import { BadgeEmailPlan } from "@/components/BadgeEmailPlan";
 import {
   EmailInfos,
   memberSchemaType,
@@ -277,8 +276,7 @@ export default function EmailContainer({
       )}
       {(emailInfos?.emailPlan === EMAIL_PLAN_TYPE.EMAIL_PLAN_BASIC ||
         emailInfos?.emailPlan === EMAIL_PLAN_TYPE.EMAIL_PLAN_PRO ||
-        userInfos.primary_email_status ==
-          EMAIL_STATUS_READABLE_FORMAT.EMAIL_DELETED) &&
+        userInfos.primary_email_status == EmailStatusCode.EMAIL_DELETED) &&
         userInfos.secondary_email && (
           /* affiche la migration dimail que si c'est un email non dimail et que c'est l'utilisateur lui-même */
           <DimailEmailCreationInvite
