beb9ed093417a0b391debe7b1e58d09151e9fcac
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2026-02-19 12:05:46 +0100

    Simplify email creation instructions

--- src/server/views/templates/emails/EmailCreatedEmail/EmailCreatedEmail.tsx
+++ src/server/views/templates/emails/EmailCreatedEmail/EmailCreatedEmail.tsx
@@ -18,8 +18,7 @@ export function EmailCreatedEmail({
     <StandardLayout title={title}>
       <MjmlText>
         <h1>Ton email {email} est prêt !</h1>
-
-        <p>Et maintenant ? Définis un nouveau mot de passe </p>
+        <p>Et maintenant ?</p>
         <ul>
           <li>
             Étape 1 : Connecte toi sur{" "}
@@ -27,13 +26,7 @@ export function EmailCreatedEmail({
             {secondaryEmail}
           </li>
           <li>
-            Étape 2 : Definis ton mot de passe pour ton adresse @beta.gouv.fr{" "}
-            <a href={`${secretariatUrl}/account#password`}>
-              (Compte {">"} Changer mot de passe)
-            </a>
-          </li>
-          <li>
-            Etape 3 : Connecte toi à ta boite email @beta.gouv.fr sur le{" "}
+            Etape 2 : Connecte toi à ta boite email @beta.gouv.fr sur le{" "}
             <a href={emailUrl}>Webmail</a>. Si tu souhaites utiliser un autre
             outil (Gmail, Outlook, …) tu peux le faire en suivant{" "}
             <a href="https://doc.incubateur.net/communaute/les-outils-de-la-communaute/emails">
@@ -42,18 +35,6 @@ export function EmailCreatedEmail({
             .
           </li>
         </ul>
-
-        <p>
-          Ton compte Mattermost (l'outil de discussion de la communauté) va être
-          automatiquement créé d'ici 10 à 15 mins, tu recevras sur {email} les
-          instructions pour y accéder.
-        </p>
-
-        <p>
-          Dans les prochains jours tu recevras un email pour te présenter les
-          prochaines étapes de ton embarquement au sein de la communauté.
-        </p>
-
         <p>Bonne journée</p>
       </MjmlText>
       <MjmlText></MjmlText>
