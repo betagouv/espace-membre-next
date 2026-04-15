04e24a32287a648bcb14ce399da36f6f6d0f69f4
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2026-04-15 08:55:47 +0200

    tests: add e2e tests for dashboard and onboarding/offboarding (#1299)
    * tests: add e2e tests for dashboard and onboarding/offboarding
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- e2e/auth.valid.member.setup.ts
+++ e2e/auth.valid.member.setup.ts
@@ -3,36 +3,36 @@ import { test as setup, expect } from "@playwright/test";
 const validMemberFile = "./playwright-auth-valid.member.json";
 
 setup("authenticate as valid.member through magic link", async ({ page }) => {
-    // fill login form
-    await page.goto("/");
-    await page.getByLabel("Mon email").fill("valid.member@betagouv.ovh");
-    await page.getByText("Recevoir le lien de connexion").click();
+  // fill login form
+  await page.goto("/login");
+  await page.getByLabel("Mon email").fill("valid.member@betagouv.ovh");
+  await page.getByText("Recevoir le lien de connexion").click();
 
-    // wait a while to get the latest email
-    await page.waitForTimeout(1000);
+  await expect(
+    page
+      .getByText(
+        "Un email avec un lien de connexion a été envoyé à ton adresse.",
+      )
+      .first(),
+  ).toBeVisible();
 
-    // open maildev and click the first invitation link
-    await page.goto("http://127.0.0.1:1080");
-    await page
-        .getByText("Connexion à l'espace membre BetaGouv")
-        .first()
-        .click();
+  // open maildev and click the first invitation link
+  await page.goto("http://127.0.0.1:1080");
+  await page.getByText("Connexion à l'espace membre BetaGouv").first().click();
 
-    // get the magic link from the email preview
-    const iframe = await page.frameLocator(".preview-iframe").first();
-    await iframe.getByText("Me connecter", { exact: true }).click();
-    const href =
-        (await iframe
-            .getByText("Me connecter", { exact: true })
-            .getAttribute("href")) || "/";
+  // get the magic link from the email preview
+  const iframe = page.frameLocator(".preview-iframe").first();
+  const href =
+    (await iframe
+      .getByText("Me connecter", { exact: true })
+      .getAttribute("href")) || "/";
 
-    // open magic link
-    await page.goto(href);
-    await page.getByText("Me connecter").first().click();
-    await page.waitForURL("/dashboard");
-    await expect(
-        page.getByText("Gérer mon compte", { exact: true }).first()
-    ).toBeVisible();
+  // open magic link
+  await page.goto(href);
+  await page.getByText("Me connecter").first().click();
+  await expect(
+    page.getByText("Gérer mon compte", { exact: true }).first(),
+  ).toBeVisible();
 
-    await page.context().storageState({ path: validMemberFile });
+  await page.context().storageState({ path: validMemberFile });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:8100",
+    baseURL: process.env.CI ? "http://127.0.0.1:8100" : "http://localhost:8100",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -34,8 +34,8 @@ export default defineConfig({
     // we'd need custom CSP
     bypassCSP: true,
   },
-  timeout: process.env.ci ? 60000 : 20000,
-  expect: { timeout: process.env.ci ? 20000 : 10000 },
+  timeout: process.env.ci ? 60000 : 30000,
+  expect: { timeout: process.env.ci ? 30000 : 20000 },
   /* Configure projects for major browsers */
   projects: [
     { name: "setup", testMatch: /.*\.setup\.ts/ },
