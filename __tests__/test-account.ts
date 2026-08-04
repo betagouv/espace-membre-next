efc42e43c15de786eee3a9d7ea1d1eb4e1be4fc9
Julien Bouquillon <julien@bouquillon.com>
2026-08-04 15:01:46 +0200

    fix

--- __tests__/test-account.ts
+++ __tests__/test-account.ts
@@ -3,6 +3,7 @@ import chaiHttp from "chai-http";
 import * as nextAuth from "next-auth/next";
 import sinon from "sinon";
 
+import utils from "./utils";
 import { createData, deleteData } from "./utils/fakeData";
 import { testUsers } from "./utils/users-data";
 import { updateCommunicationEmail } from "@/app/api/member/actions";
