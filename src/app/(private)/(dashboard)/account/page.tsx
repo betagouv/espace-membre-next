8d6761a6a17d17c27174751a8d452e04a858839b
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2026-08-04 22:30:20 +0200

    chore: cleanup fiches membres (#1485)
    * chore: simplify member props
    
    * chore: simplify baseInfoUpdate
    
    * fix
    
    * fix
    
    * restore

--- src/app/(private)/(dashboard)/account/page.tsx
+++ src/app/(private)/(dashboard)/account/page.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
+import type { Session } from "next-auth";
 
-import { getUserInformations } from "@/app/api/member/getInfo";
-import { userInfos } from "@/server/controllers/utils";
 import { authOptions } from "@/utils/authoptions";
 import { routeTitles } from "@/utils/routes/routeTitles";
-import { getUserIncubators } from "@/lib/kysely/queries/users";
-import { getUserChecklists } from "@/utils/checklists/getUserChecklists";
+import { buildMemberPageProps } from "@/lib/memberPageProps";
 import MemberPage from "@/components/MemberPage/MemberPage";
 
 export const metadata: Metadata = {
@@ -15,50 +13,16 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  // todo: merge with community/id/page
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions) as Session;
   if (!session) {
     redirect("/login");
   }
 
-  const id = session?.user?.id;
+  const memberPageProps = await buildMemberPageProps({
+    session,
+    memberId: session.user.id,
+    isCurrentUser: true,
+  });
 
-  // compile some account informations
-  const user = await userInfos({ username: id }, session.user.id === id);
-
-  // compile some other infos
-  const userInformations = await getUserInformations(id);
-
-  if (!userInformations) {
-    throw new Error("Cannot find user");
-  }
-
-  const isAdmin = !!session.user.isAdmin;
-
-  const { onboarding, offboarding } = await getUserChecklists(
-    session.user.uuid,
-  );
-
-  const incubators = await getUserIncubators(userInformations.baseInfo.uuid);
-
-  return (
-    <MemberPage
-      isAdmin={isAdmin}
-      authorizations={user.authorizations}
-      emailInfos={user.emailInfos}
-      emailRedirections={user.emailRedirections}
-      isExpired={user.isExpired}
-      avatar={userInformations.avatar} // todo
-      changes={userInformations.changes}
-      userInfos={userInformations.baseInfo}
-      matrixId={userInformations.matrixId}
-      startups={userInformations.startups}
-      canEditMember={true}
-      canValidateMember={false}
-      isCurrentUser={true}
-      onboarding={onboarding}
-      offboarding={offboarding}
-      incubators={incubators}
-    />
-  );
+  return <MemberPage {...memberPageProps} />;
 }
---
+++ src/lib/memberPageProps.ts
@@ -0,0 +1,83 @@
+import { MemberPageProps } from "@/components/MemberPage/MemberPage";
+import { userInfos } from "@/server/controllers/utils";
+import { canEditMember as _canEditMember } from "@/lib/canEditMember";
+import { getUserChecklists } from "@/utils/checklists/getUserChecklists";
+import {
+  getUserStartups,
+  getUserIncubators,
+  getUserBasicInfo,
+} from "@/lib/kysely/queries/users";
+import { getEventListByUsername } from "@/lib/events";
+import { getAvatarUrl } from "@/lib/s3";
+import { db } from "@/lib/kysely";
+import { memberChangeToModel, memberBaseInfoToModel } from "@/models/mapper";
+import type { Session } from "next-auth";
+
+type PageContext = {
+  session: Session;
+  memberId: string;
+  isCurrentUser: boolean;
+};
+
+export async function buildMemberPageProps({
+  session,
+  memberId,
+  isCurrentUser,
+}: PageContext): Promise<MemberPageProps> {
+  const [user, dbUser] = await Promise.all([
+    userInfos({ username: memberId }, isCurrentUser),
+    getUserBasicInfo({ username: memberId }),
+  ]);
+
+  if (!dbUser) {
+    throw new Error("Cannot find user");
+  }
+
+  const baseInfo = memberBaseInfoToModel(dbUser);
+
+  const [changes, avatar, startups, matrixAccount, { onboarding, offboarding }, incubators] =
+    await Promise.all([
+      getEventListByUsername(memberId).then((events) =>
+        events.map(memberChangeToModel),
+      ),
+      getAvatarUrl(dbUser.username),
+      getUserStartups(dbUser.uuid),
+      db
+        .selectFrom("matrix_accounts")
+        .select("matrix_id")
+        .where("user_id", "=", dbUser.uuid)
+        .executeTakeFirst(),
+      getUserChecklists(user.userInfos.uuid),
+      getUserIncubators(dbUser.uuid),
+    ]);
+
+  const isAdmin = !!session.user.isAdmin;
+  const canEditMember = isCurrentUser
+    ? true
+    : await _canEditMember({
+        memberUuid: user.userInfos.uuid,
+        sessionUser: session.user,
+      });
+
+  const canValidateMember =
+    canEditMember && session.user.uuid !== user.userInfos.uuid;
+
+  return {
+    isAdmin,
+    isCurrentUser,
+    canEditMember,
+    canValidateMember,
+    authorizations: user.authorizations,
+    emailInfos: user.emailInfos,
+    emailRedirections: user.emailRedirections,
+    isExpired: user.isExpired,
+    avatar,
+    changes,
+    userInfos: baseInfo,
+    matrixId: matrixAccount?.matrix_id,
+    startups,
+    onboarding,
+    offboarding,
+    incubators,
+  };
+}
