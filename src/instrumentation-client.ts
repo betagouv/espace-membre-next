b6fd213505034f3d015aa3d7885336a755c58451
Julien Bouquillon <julien@bouquillon.com>
2026-08-07 23:20:57 +0200

    chore: upgrade sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,12 +3,13 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { replayIntegration } from "@sentry/react";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  integrations: [replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 0.5,
