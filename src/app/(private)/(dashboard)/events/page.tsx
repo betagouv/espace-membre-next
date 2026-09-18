d599b971c4608dd7aeac6f023be7216fd92876ad
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2026-09-18 19:24:06 +0200

    fix: Calendar (#1546)
    * fix: use new calendar url
    
    * fix

--- src/app/(private)/(dashboard)/events/page.tsx
+++ src/app/(private)/(dashboard)/events/page.tsx
@@ -8,14 +8,11 @@ import { parseICS } from "@/lib/ical";
 
 import { EventsList } from "@/components/EventsList/EventsList";
 import { authOptions } from "@/lib/authoptions";
-
+import config from "@/lib/config";
 
 export const metadata: Metadata = {
   title: `${routeTitles.eventsList()} / Espace Membre`,
 };
-
-const calendarUrl =
-  "https://calendar.google.com/calendar/ical/0ieonqap1r5jeal5ugeuhoovlg%40group.calendar.google.com/public/basic.ics";
 
 const fetchCalendar = (url) => {
   return fetch(url)
@@ -25,7 +22,7 @@ const fetchCalendar = (url) => {
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  const events = await fetchCalendar(calendarUrl);
+  const events = await fetchCalendar(config.CALENDAR_PUBLIC_URL);
 
   if (!session) {
     redirect("/login");
@@ -35,24 +32,17 @@ export default async function Page() {
       <h1 className={fr.cx("fr-mb-6w")}>
         {routeTitles.eventsList()}{" "}
         <span style={{ fontSize: "1rem" }}>
-          <Link
-            className={fr.cx("fr-ml-2w")}
-            href="https://calendar.google.com/calendar/ical/0ieonqap1r5jeal5ugeuhoovlg%40group.calendar.google.com/public/basic.ics"
-            target="_blank"
-            title="Fichier iCal de l'agenda public - ouvre une nouvelle fenêtre"
-          >
-            <span className={fr.cx("fr-icon-download-line")}></span>
-            Lien vers le fichier iCal
-          </Link>
-          <Link
-            className={fr.cx("fr-ml-2w")}
-            href="https://calendar.google.com/calendar/embed?src=0ieonqap1r5jeal5ugeuhoovlg%40group.calendar.google.com&ctz=Europe%2FParis"
-            target="_blank"
-            title="Agenda public - ouvre une nouvelle fenêtre"
-          >
-            <span className={fr.cx("fr-icon-calendar-2-line")}></span>
-            Lien vers l'agenda public
-          </Link>
+          {config.CALENDAR_PUBLIC_URL && (
+            <Link
+              className={fr.cx("fr-ml-2w")}
+              href={config.CALENDAR_PUBLIC_URL}
+              target="_blank"
+              title="Lien vers l'agenda public - ouvre une nouvelle fenêtre"
+            >
+              <span className={fr.cx("fr-icon-calendar-2-line")}></span>
+              Lien vers l'agenda public
+            </Link>
+          )}
         </span>
       </h1>
       <EventsList events={events}></EventsList>
